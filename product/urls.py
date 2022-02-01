8bafef00b7dee090c3ba56fcd4a4d8fc63b746ef
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 15:57:40 +0300

    - Map api view 'ListProductAPIView' to a 'URLConf'.

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,8 @@
+from product.views.product import ListProductAPIView
 from product.views.category import ListProductCategoryAPIView
 from django.urls.conf import path
 
 urlpatterns = [
     path('list/category/', ListProductCategoryAPIView.as_view()),
+    path('list/product/', ListProductAPIView.as_view()),
 ]
