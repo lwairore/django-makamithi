34e94015fd6dc6c0661b9371695ed89e90cf36bd
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 17:27:00 +0300

    - Create model serializer '_ListPriceModelSerializer'.

--- product/serializers/product.py
+++ product/serializers/product.py
@@ -1,6 +1,13 @@
+from product.submodels.price import PriceModel
 from rest_framework.fields import CharField
 from product.submodels.product import ProductModel
 from rest_framework.serializers import ModelSerializer
+
+
+class _ListPriceModelSerializer(ModelSerializer):
+    class Meta:
+        model = PriceModel
+        fields = ('was', 'now', 'per',)
 
 
 class ListProductSerializer(ModelSerializer):
