e5c51bc95899f4016f899c0757efd3e96ea10da9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 16:40:29 +0300

    - Overwrite how we get the value for field 'category'.

--- product/serializers/product.py
+++ product/serializers/product.py
@@ -1,8 +1,11 @@
+from rest_framework.fields import CharField
 from product.submodels.product import ProductModel
 from rest_framework.serializers import ModelSerializer
 
 
 class ListProductSerializer(ModelSerializer):
+    category = CharField(source='category.title')
+
     class Meta:
         model = ProductModel
         fields = ('title', 'category',)
