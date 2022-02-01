7b72d9a9b63bb7630768168f34d50826b3ce8a39
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 13:56:48 +0300

    - Update api view 'ListProductCategoryAPIView'.

--- product/views.py
+++ product/views.py
@@ -1,9 +1,23 @@
+from product.serializers import ListProductCategorySerializer
+from product.submodels.category import ProductCategoryModel
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 
 class ListProductCategoryAPIView(APIView):
     permission_classes = (AllowAny,)
+    _category_model_class = ProductCategoryModel
+    _serializer_class = ListProductCategorySerializer
+
+    def list_product_category(self):
+        categories = self._category_model_class.objects.all()
+
+        return categories
 
     def get(self, request):
-        pass
+        categories = self.list_product_category()
+
+        categories_serializer = self._serializer_class(
+            categories, many=True)
+
+        return categories_serializer.data
