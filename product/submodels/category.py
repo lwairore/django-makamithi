8ceefe180dd105404d69ad51984f325a83d464c3
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 17:14:50 +0300

    - Add field created_at to productcategorymodel     - Add field modified_date to productcategorymodel

--- product/submodels/category.py
+++ product/submodels/category.py
@@ -1,10 +1,12 @@
 from django.db.models import Model
-from django.db.models.fields import (CharField, TextField)
+from django.db.models.fields import (CharField, DateTimeField, TextField)
 
 
 class ProductCategoryModel(Model):
     title = CharField(max_length=250)
     description = TextField(max_length=5000, blank=True, null=True)
+    created_at    = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.title
