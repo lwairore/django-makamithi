6552f0e5d810961c1cc6dbb38162e762362d7519
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 17:16:12 +0300

    - Add field created_at to productmodel     - Add field modified_date to productmodel

--- product/submodels/product.py
+++ product/submodels/product.py
@@ -1,5 +1,6 @@
 from django.db.models import (
     Model, ForeignKey, CharField, PROTECT, ManyToManyField)
+from django.db.models.fields import DateTimeField
 
 
 class ProductModel(Model):
@@ -7,6 +8,8 @@ class ProductModel(Model):
     category = ForeignKey('ProductCategoryModel', on_delete=PROTECT,
                           blank=True, null=True, related_name='product_category')
     price = ManyToManyField('PriceModel', blank=True)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.title
