7835089fd09e08b1821f4caa085fa69af982a126
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 17:15:04 +0300

    - Format document.

--- product/submodels/category.py
+++ product/submodels/category.py
@@ -5,7 +5,7 @@ from django.db.models.fields import (CharField, DateTimeField, TextField)
 class ProductCategoryModel(Model):
     title = CharField(max_length=250)
     description = TextField(max_length=5000, blank=True, null=True)
-    created_at    = DateTimeField(auto_now_add=True, blank=True, null=True)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
     modified_date = DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self) -> str:
