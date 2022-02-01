6ed8be8a5e2053fd7acacd50a93a8717caaf097b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 13:49:53 +0300

    - Create class 'ListProductCategoryAPIView'.

--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class ListProductCategoryAPIView(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request):
+        pass
