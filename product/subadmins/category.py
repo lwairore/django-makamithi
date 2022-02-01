829424af5d71fcdc609a5b2f72e81dcc6e8a3f43
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 16:51:57 +0300

    - Update model 'ProductCategoryModelAdmin'.

--- product/subadmins/category.py
+++ product/subadmins/category.py
@@ -1,8 +1,16 @@
+from product.submodels.product import ProductModel
 from product.submodels.category import ProductCategoryModel
 from django.contrib.admin import ModelAdmin, register
+from django.db.models import Count
 
 
 @register(ProductCategoryModel)
 class ProductCategoryModelAdmin(ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'number_of_products',)
     search_fields = ('title', 'description',)
+
+    def number_of_products(self, obj):
+        products_count = obj.product_category.all()\
+            .order_by().only('id').count()
+
+        return products_count
