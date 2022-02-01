75547cf469d0dc5e855871e95bf70f3d726384cc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 15:34:23 +0300

    - Register model 'Product' onto the admin site.

---
+++ product/subadmins/product.py
@@ -0,0 +1,13 @@
+from product.submodels.product import ProductModel
+from django.contrib.admin import ModelAdmin, register
+
+
+@register(ProductModel)
+class ProductModelAdmin(ModelAdmin):
+    list_display = ('title',)
+    raw_id_fields = ('category',)
+    fieldsets = (
+        (None, {
+            'fields': ('title',)
+        })
+    )
