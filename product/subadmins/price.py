f7984f2af2bf8cdc76c2ccb475794a476cea4721
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 17:35:21 +0300

    - Update 'PriceModelAdmin'.

--- product/subadmins/price.py
+++ product/subadmins/price.py
@@ -5,6 +5,7 @@ from django.contrib.admin import ModelAdmin, register
 @register(PriceModel)
 class PriceModelAdmin(ModelAdmin):
     list_display = ('now', 'was', 'per',)
+    raw_id_fields = ('per',)
     fieldsets = (
         (None, {
             'fields': ('now', 'was', 'per',)
