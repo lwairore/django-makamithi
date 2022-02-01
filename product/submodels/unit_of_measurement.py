8d202ebfca710c33b19af0b7cc3220ee9e98e4f8
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-01 17:16:54 +0300

    - Add field created_at to unitofmeasurementmodel     - Add field modified_date to unitofmeasurementmodel

--- product/submodels/unit_of_measurement.py
+++ product/submodels/unit_of_measurement.py
@@ -1,9 +1,12 @@
 from django.db.models import (Model, CharField, TextField)
+from django.db.models.fields import DateTimeField
 
 
 class UnitOfMeasurementModel(Model):
     title = CharField(max_length=60)
     description = TextField(max_length=5000, blank=True, null=True)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.title
