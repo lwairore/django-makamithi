7090ace2ddf20d717da863e90de7de694f12f39c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-03 14:08:08 +0300

    - Map api view 'ListProductReviewAPIView' to a 'URLConf'.

--- product/suburls/reviews.py
+++ product/suburls/reviews.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from product.views.product_review import ListProductReviewAPIView
+from django.urls import path
+
+urlpatterns = [
+    path('list/product/<int:product_id>/', ListProductReviewAPIView.as_view()),
+]
