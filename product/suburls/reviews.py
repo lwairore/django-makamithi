e7b3991cef0230dfa8679283882fd2653eee58b9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-03 14:12:46 +0300

    - Initialize constant 'REVIEW_URL_FILE_NAME_WITHOUT_EXTENSION'.

--- product/suburls/reviews.py
+++ product/suburls/reviews.py
@@ -1,5 +1,8 @@
+from custom_utils.get_file_name_util import get_file_name
 from product.views.product_review import ListProductReviewAPIView
 from django.urls import path
+
+REVIEW_URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 urlpatterns = [
     path('list/product/<int:product_id>/', ListProductReviewAPIView.as_view()),
