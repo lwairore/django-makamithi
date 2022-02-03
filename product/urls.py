206eba39d9df7d480b06853ef7d42f1073ed5bd8
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-03 14:22:40 +0300

    - Update list 'urlpatterns'.

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,14 @@
+from product.suburls.reviews import REVIEW_URL_FILE_NAME_WITHOUT_EXTENSION
+from product.suburls import URL_FILE_NAME
+from product.apps import ProductConfig
 from product.views.product import ListProductAPIView
 from product.views.category import ListProductCategoryAPIView
-from django.urls.conf import path
+from django.urls.conf import path, include
 
 urlpatterns = [
     path('list/category/', ListProductCategoryAPIView.as_view()),
     path('list/product/', ListProductAPIView.as_view()),
+    path('reviews/',
+         include(f'{ProductConfig.name}.{URL_FILE_NAME}.{REVIEW_URL_FILE_NAME_WITHOUT_EXTENSION}')),
+
 ]
