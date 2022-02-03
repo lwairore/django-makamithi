18e9691d6a52dfaca38d1104d3a06da81853fb6d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-03 16:21:26 +0300

    - Define method 'post' on 'ListUpdateProductReviewAPIView'.

--- product/views/product_review.py
+++ product/views/product_review.py
@@ -1,16 +1,20 @@
+from product.serializers.details_to_note_serializers import DetailToNoteSerializer
+from custom_utils.iterable_object_is_not_empty import iterable_object_is_not_empty
+from custom_utils.parse_json_data_util import parse_json_data
+from custom_utils.string_is_not_empty import string_is_not_empty
+from custom_utils.check_key_util import check_key
 from django.db.models.query import Prefetch
 from product.submodels.product import ProductModel
 from product.submodels.rating_scale import ProductReviewModel
-from product.serializers.product_review import ListProductReviewSerializer
+from product.serializers.product_review import ListProductReviewSerializer, UpdateProductReviewGatewaySerializer, UpdateProductReviewSerializer
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.status import HTTP_404_NOT_FOUND
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 
 
 class ListUpdateProductReviewAPIView(APIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
-    _serializer_class = ListProductReviewSerializer
 
     def _get_product_instance(self, product_id: int):
         product_instance = None
@@ -46,6 +50,58 @@ class ListUpdateProductReviewAPIView(APIView):
 
         reviews = self._list_reviews(product_instance)
 
-        reviews_serializer = self._serializer_class(reviews, many=True)
+        reviews_serializer = ListProductReviewSerializer(reviews, many=True)
 
         return Response(reviews_serializer.data)
+
+    def post(self, request, product_id: int):
+        product_instance = None
+
+        try:
+            product_instance = self._get_product_instance(product_id)
+
+        except ProductModel.DoesNotExist:
+            return Response({'detail': f'Product with that id {product_id} not found.'}, status=HTTP_404_NOT_FOUND)
+
+        request_data = request.data
+
+        if not check_key(request_data, 'reviews'):
+            return Response({'detail': 'Nothing TODO here'})
+
+        unformatted_reviews = request_data.get('contact_details')
+
+        if not string_is_not_empty(unformatted_reviews):
+            return Response({'detail': 'Nothing TODO here'})
+
+        print(unformatted_reviews)
+
+        unformatted_reviews = parse_json_data(unformatted_reviews)
+
+        if not iterable_object_is_not_empty(unformatted_reviews):
+            return Response({'detail': 'Nothing TODO here'})
+
+        formatted_reviews = UpdateProductReviewSerializer()\
+            .format_request_data(unformatted_reviews)
+
+        if not iterable_object_is_not_empty(formatted_reviews):
+            return Response({'detail': 'Nothing TODO here'})
+
+        update_reviews_serializer = UpdateProductReviewGatewaySerializer(
+            instance=product_instance,
+            data={'reviews': formatted_reviews})
+
+        if update_reviews_serializer.is_valid():
+            details_to_note = update_reviews_serializer.save()
+
+            if iterable_object_is_not_empty(details_to_note):
+                details_to_note_serializer = DetailToNoteSerializer(
+                    details_to_note)
+
+                response_data = details_to_note_serializer.data
+
+                return Response(response_data)
+            else:
+                return Response(data={})
+        else:
+            return Response(update_reviews_serializer.errors,
+                            status=HTTP_400_BAD_REQUEST)
