2be32a656b9feed04f0e0304cb2ac8edd0c54c6c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-03 16:20:18 +0300

    - Add needed serializer.

---
+++ product/serializers/details_to_note_serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.fields import IntegerField
+from rest_framework.serializers import Serializer
+
+
+class DetailToNoteSerializer(Serializer):
+    loop_index_for_fr_updating = IntegerField()
+    id = IntegerField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
