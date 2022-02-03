41a45d8ec6be9b21eb7e6a623242b6554752a142
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-03 16:05:40 +0300

    - Update serializer 'UpdateProductReviewSerializer'.

--- product/serializers/product_review.py
+++ product/serializers/product_review.py
@@ -1,3 +1,5 @@
+from django.db.models.query import QuerySet
+from rest_framework.exceptions import ValidationError
 from product.submodels.product import ProductModel
 from custom_utils.dict_is_not_empty import dict_is_not_empty
 from custom_utils.string_is_not_empty import string_is_not_empty
@@ -12,6 +14,18 @@ class ListProductReviewSerializer(ModelSerializer):
     class Meta:
         model = ProductReviewModel
         fields = ('rating', 'review',)
+
+
+class _AddProductReviewSerializer(ModelSerializer):
+
+    class Meta:
+        model = ProductReviewModel
+        fields = ('rating', 'review',)
+
+    def create(self, validated_data):
+        new_review = ProductReviewModel.objects.create(**validated_data)
+
+        return new_review
 
 
 class UpdateProductReviewSerializer(ModelSerializer):
@@ -33,6 +47,52 @@ class UpdateProductReviewSerializer(ModelSerializer):
                 formatted_review['review'] = value_for_review
 
         return formatted_review if dict_is_not_empty(formatted_review) else None
+
+    def manually_handle_put_for_non_existing_models(self, product_instance: ProductModel,  updated_details):
+        new_review_serializer = _AddProductReviewSerializer(
+            data=updated_details)
+
+        if not new_review_serializer.is_valid():
+            raise ValidationError(detail=new_review_serializer.errors)
+
+        new_review = new_review_serializer.save()
+
+        product_instance.reviews.add(new_review)
+
+        return new_review
+
+    def manually_handle_put_for_existing_models(self, product_instance: ProductModel, updated_details):
+        if not check_key(updated_details, 'id'):
+            return product_instance
+
+        review_id = updated_details.get('id')
+
+        review_kueryset: QuerySet = product_instance.reviews\
+            .filter(id=review_id)
+
+        if not review_kueryset.exists():
+            raise ValidationError({
+                'reviews':
+                    [f'Object with id={review_id} does not exist.']
+            })
+
+        review_instance: ProductReviewModel = review_kueryset.first()
+
+        if check_key(updated_details, 'rating'):
+            updated_rating = updated_details.get('rating')
+
+            if updated_rating != review_instance.rating:
+                review_instance.rating = updated_rating
+                review_instance.save(update_fields=['rating', ])
+
+        if check_key(updated_details, 'review'):
+            updated_review = updated_details.get('review')
+
+            if updated_review != review_instance.review:
+                review_instance.review = updated_review
+                review_instance.save(update_fields=['review', ])
+
+        return review_instance
 
     class Meta:
         model = ProductReviewModel
