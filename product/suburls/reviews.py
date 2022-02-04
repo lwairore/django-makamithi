1c262ac721bdf453aac86470d898b080f780e7a0
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-04 10:44:02 +0300

    - Add a URLConf for adding a review.

--- product/suburls/reviews.py
+++ product/suburls/reviews.py
@@ -6,6 +6,12 @@ REVIEW_URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _LIST_PRODUCT_REVIEW_URL_ROUTE = ''
 
+_ADD_PRODUCT_REVIEW_URL_ROUTE = 'add/'
+
 urlpatterns = [
+    # List reviews for a particular product
     path(_LIST_PRODUCT_REVIEW_URL_ROUTE, ListUpdateProductReviewAPIView.as_view()),
+
+    # Add review for a product
+    path(_ADD_PRODUCT_REVIEW_URL_ROUTE, ListUpdateProductReviewAPIView.as_view()),
 ]
