4bc19ec6fc19338abf8e76a4c582768f747f518c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-04 11:04:28 +0300

    - Make field 'loop_index_for_fr_updating' to be 'optional'.

--- product/serializers/details_to_note_serializers.py
+++ product/serializers/details_to_note_serializers.py
@@ -3,7 +3,7 @@ from rest_framework.serializers import Serializer
 
 
 class DetailToNoteSerializer(Serializer):
-    loop_index_for_fr_updating = IntegerField()
+    loop_index_for_fr_updating = IntegerField(required=False)
     id = IntegerField()
 
     def create(self, validated_data):
