e04017ed7fe21af586bc4e084943254c215b6434
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-04 10:40:39 +0300

    - Initialize constant '_PRODUCT_ID_PARAMETER'. - Update the base url for reviews.

--- product/urls.py
+++ product/urls.py
@@ -5,10 +5,11 @@ from product.views.product import ListProductAPIView
 from product.views.category import ListProductCategoryAPIView
 from django.urls.conf import path, include
 
+_PRODUCT_ID_PARAMETER = '<int:product_id>'
+
 urlpatterns = [
     path('list/category/', ListProductCategoryAPIView.as_view()),
     path('list/product/', ListProductAPIView.as_view()),
-    path('reviews/',
+    path(f'{_PRODUCT_ID_PARAMETER}/reviews/',
          include(f'{ProductConfig.name}.{URL_FILE_NAME}.{REVIEW_URL_FILE_NAME_WITHOUT_EXTENSION}')),
-
 ]
