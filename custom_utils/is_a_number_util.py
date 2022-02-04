4bfb1521d3ffd4405aa16abb3ab7d995971d3707
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-04 14:24:52 +0300

    - Define method 'is_a_number'.

--- custom_utils/is_a_number_util.py
+++ custom_utils/is_a_number_util.py
@@ -15,3 +15,16 @@ def is_integer(value):
 
 def is_float(value):
     return bool(re.match(_FLOAT_REGEX, value))
+
+
+def is_a_number(value, number_type=INTEGER_TYPE):
+    if type(value) != str:
+        value = str(value)
+
+    if number_type == INTEGER_TYPE:
+        return is_integer(value)
+    elif number_type == FLOAT_TYPE:
+        return is_float(value)
+    else:
+        raise ValueError(
+            'Value for argument \'number_type\' is not recognized.')
