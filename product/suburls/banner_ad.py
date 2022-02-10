b98573b7165cdc0558b7b7ac1359b55dface11d0
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 11:00:05 +0300

    - Initialize empty list 'urlpatterns'.

--- product/suburls/banner_ad.py
+++ product/suburls/banner_ad.py
@@ -3,3 +3,7 @@ from custom_utils.get_file_name_util import get_file_name
 BANNER_AD_URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _LIST_BANNER_AD_URL_ROUTE = ''
+
+urlpatterns = [
+
+]
