d9b7a118d06d4bd7b6c09efdd3412140185b544f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 18:13:08 +0300

    - Rename constant 'BANNER_AD_URL_FILE_NAME_WITHOUT_EXTENSION' to 'URL_FILE_NAME_WITHOUT_EXTENSION'.

--- home_two/urls.py
+++ home_two/urls.py
@@ -1,5 +1,5 @@
 from home_two.suburls.about_section import ABOUT_SECTION_ROOT_ROUTE, URL_FILE_NAME_WITHOUT_EXTENSION
-from home_two.suburls.banner_ad import BANNER_AD_ROOT_ROUTE, BANNER_AD_URL_FILE_NAME_WITHOUT_EXTENSION
+from home_two.suburls.banner_ad import BANNER_AD_ROOT_ROUTE, URL_FILE_NAME_WITHOUT_EXTENSION
 from home_two.apps import HomeTwoConfig
 from home_two.suburls import URL_FILE_NAME
 from django.urls.conf import path, include
@@ -9,7 +9,7 @@ app_name = HomeTwoConfig.name
 urlpatterns = [
     # Routes for Banner-ads
     path(BANNER_AD_ROOT_ROUTE,
-         include(f'{app_name}.{URL_FILE_NAME}.{BANNER_AD_URL_FILE_NAME_WITHOUT_EXTENSION}')),
+         include(f'{app_name}.{URL_FILE_NAME}.{URL_FILE_NAME_WITHOUT_EXTENSION}')),
 
     #  Routes for About section
     path(ABOUT_SECTION_ROOT_ROUTE,
