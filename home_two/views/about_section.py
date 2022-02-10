eabf2864fbd3a6b7378ff6ca058755676161e9a6
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 18:00:51 +0300

    - Initialize property '_serializer_class'.

--- home_two/views/about_section.py
+++ home_two/views/about_section.py
@@ -1,6 +1,8 @@
+from home_two.serializers.about_section import RetrieveAboutSectionSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 
 class RetrieveAboutSectionAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveAboutSectionSerializer
