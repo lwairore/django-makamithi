d4bd5285997658b995085b20da42644f5770c8b6
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 14:43:37 +0300

    - Update model admin 'BannerAdModelAdmin'.

--- home_two/subadmins/banner_ad.py
+++ home_two/subadmins/banner_ad.py
@@ -1,17 +1,17 @@
-from home.submodels.banner_ad import BannerAdModel
+from home_two.submodels.banner_ad import BannerAdModel
 from django.contrib.admin import ModelAdmin, register
 from django.utils.html import format_html
 
 
 @register(BannerAdModel)
 class BannerAdModelAdmin(ModelAdmin):
-    list_display = ('title',)
-    # filter_horizontal = ('photos',)
+    list_display = ('title', 'number_of_photos',)
+    filter_horizontal = ('photos',)
 
-    # def number_of_photos(self, obj: BannerAdModel):
-    #     photos_count = obj.photos.all()\
-    #         .order_by().only('id').count()
+    def number_of_photos(self, obj: BannerAdModel):
+        photos_count = obj.photos.all()\
+            .order_by().only('id').count()
 
-    #     return format_html("<b><i>{}</i></b>", photos_count)
+        return format_html("<b><i>{}</i></b>", photos_count)
 
-    # number_of_photos.short_description = 'Total photos'
+    number_of_photos.short_description = 'Total photos'
