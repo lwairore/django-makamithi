54d391bb243c8cbdc956da8cfbcaff74ee0b9ee2
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 10:41:43 +0300

    - Alter field description on banneradmodel

--- product/submodels/banner_ad.py
+++ product/submodels/banner_ad.py
@@ -5,7 +5,7 @@ from django.db.models.fields.related import ManyToManyField
 
 class BannerAdModel(Model):
     title = CharField(max_length=70)
-    description = TextField(max_length=140)
+    description = TextField(max_length=250, blank=True, null=True)
     created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
     modified_date = DateTimeField(auto_now=True, blank=True, null=True)
     photos = ManyToManyField('PhotoModel', blank=True)
