5492cb4a18a8f3250aee684d957cc7aa15ee28eb
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 11:36:00 +0300

    - Comment out line to avoid django migrations from occuring.

--- product/submodels/__init__.py
+++ product/submodels/__init__.py
@@ -1,6 +1,6 @@
-from .category import ProductCategoryModel
-from .price import PriceModel
-from .product import ProductModel
-from .unit_of_measurement import UnitOfMeasurementModel
-from .rating_scale import ProductReviewModel
-from .preview_item import PhotoModel
+# from .category import ProductCategoryModel
+# from .price import PriceModel
+# from .product import ProductModel
+# from .unit_of_measurement import UnitOfMeasurementModel
+# from .rating_scale import ProductReviewModel
+# from .preview_item import PhotoModel
