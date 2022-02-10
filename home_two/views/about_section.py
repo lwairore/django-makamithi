1c2696a51e47d05a941f16dd53ae8669c145930b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 18:02:41 +0300

    - Define method '_get_about_section_instance'.

--- home_two/views/about_section.py
+++ home_two/views/about_section.py
@@ -1,3 +1,4 @@
+from home_two.submodels.about_section import AboutSectionModel
 from home_two.serializers.about_section import RetrieveAboutSectionSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
@@ -6,3 +7,12 @@ from rest_framework.views import APIView
 class RetrieveAboutSectionAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveAboutSectionSerializer
+
+    def _get_about_section_instance(self):
+        about_section_instance = AboutSectionModel.objects\
+            .select_related('photo')\
+            .order_by()\
+            .only('title', 'description', 'photo__image',
+                  'photo__caption').first()
+
+        return about_section_instance
