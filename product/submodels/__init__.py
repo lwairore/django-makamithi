1667d61dd4655afae91bca24aab8b969e44b8336
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 09:52:15 +0300

    - Create model PhotoModel.

--- product/submodels/__init__.py
+++ product/submodels/__init__.py
@@ -4,3 +4,4 @@ from .product import ProductModel
 from .unit_of_measurement import UnitOfMeasurementModel
 from .rating_scale import ProductReviewModel
 from .banner_ad import BannerAdModel
+from .preview_item import PhotoModel
---
+++ product/submodels/preview_item.py
@@ -0,0 +1,19 @@
+from django.db.models.fields.files import ImageField
+from custom_utils.rename_image_filename_util import rename_uploaded_image_and_path
+from django.db.models import Model
+from django.db.models.fields import DateTimeField, TextField
+
+
+class PhotoModel(Model):
+    image = ImageField(upload_to=rename_uploaded_image_and_path,
+                       max_length=2000)
+    caption = TextField(max_length=5000, blank=True, null=True)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f'Image ID={self.id}'
+
+    class Meta:
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
