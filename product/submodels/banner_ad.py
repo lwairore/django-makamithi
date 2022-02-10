e0a2270c4b2d79b5f2002588ee6391ec74e70cd0
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 09:46:26 +0300

    - Add field created_at to banneradmodel

--- product/submodels/banner_ad.py
+++ product/submodels/banner_ad.py
@@ -1,10 +1,11 @@
 from django.db.models import Model
-from django.db.models.fields import CharField, TextField
+from django.db.models.fields import CharField, DateTimeField, TextField
 
 
 class BannerAdModel(Model):
     title = CharField(max_length=70)
     description = TextField(max_length=140)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.title
