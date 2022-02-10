315209b80479b4712a3853ab30b94a8c045e4e07
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 18:09:00 +0300

    - Refactor list 'urlpatterns'.

--- home_two/urls.py
+++ home_two/urls.py
@@ -1,4 +1,4 @@
-from home_two.suburls.banner_ad import BANNER_AD_URL_FILE_NAME_WITHOUT_EXTENSION
+from home_two.suburls.banner_ad import BANNER_AD_ROOT_ROUTE, BANNER_AD_URL_FILE_NAME_WITHOUT_EXTENSION
 from home_two.apps import HomeTwoConfig
 from home_two.suburls import URL_FILE_NAME
 from django.urls.conf import path, include
@@ -6,6 +6,6 @@ from django.urls.conf import path, include
 app_name = HomeTwoConfig.name
 
 urlpatterns = [
-    path('list/banner-ad/',
+    path(BANNER_AD_ROOT_ROUTE,
          include(f'{app_name}.{URL_FILE_NAME}.{BANNER_AD_URL_FILE_NAME_WITHOUT_EXTENSION}')),
 ]
