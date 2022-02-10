11643bd2a91cf8a003bb2a5713abd00bcf3935dc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 11:35:37 +0300

    - Comment out line to avoid django migrations from occuring.

--- product/urls.py
+++ product/urls.py
@@ -8,8 +8,8 @@ from django.urls.conf import path, include
 _PRODUCT_ID_PARAMETER = '<int:product_id>'
 
 urlpatterns = [
-    path('list/category/', ListProductCategoryAPIView.as_view()),
-    path('list/product/', ListProductAPIView.as_view()),
-    path(f'{_PRODUCT_ID_PARAMETER}/reviews/',
-         include(f'{ProductConfig.name}.{URL_FILE_NAME}.{REVIEW_URL_FILE_NAME_WITHOUT_EXTENSION}')),
+    # path('list/category/', ListProductCategoryAPIView.as_view()),
+    # path('list/product/', ListProductAPIView.as_view()),
+    # path(f'{_PRODUCT_ID_PARAMETER}/reviews/',
+    #      include(f'{ProductConfig.name}.{URL_FILE_NAME}.{REVIEW_URL_FILE_NAME_WITHOUT_EXTENSION}')),
 ]
