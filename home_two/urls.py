7028b906846056a8e36c87ba09a3df2c55fbd7aa
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 14:54:39 +0300

    - Update list 'urlpatterns'.

--- home_two/urls.py
+++ home_two/urls.py
@@ -6,6 +6,6 @@ from django.urls.conf import path, include
 app_name = HomeTwoConfig.name
 
 urlpatterns = [
-    path('banner-ad/',
+    path('list/banner-ad/',
          include(f'{app_name}.{URL_FILE_NAME}.{BANNER_AD_URL_FILE_NAME_WITHOUT_EXTENSION}')),
 ]
