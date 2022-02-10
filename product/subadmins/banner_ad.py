488c734b10fc43e8af49dd978c6418b761f7ecfa
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 10:00:45 +0300

    - Register model 'BannerAdModel' to admin site.

---
+++ product/subadmins/banner_ad.py
@@ -0,0 +1,17 @@
+from product.submodels.banner_ad import BannerAdModel
+from django.contrib.admin import ModelAdmin, register
+from django.utils.html import format_html
+
+
+@register(BannerAdModel)
+class BannerAdModelAdmin(ModelAdmin):
+    list_display = ('title', 'number_of_photos',)
+    filter_horizontal = ('photos',)
+
+    def number_of_photos(self, obj: BannerAdModel):
+        photos_count = obj.photos.all()\
+            .order_by().only('id').count()
+
+        return format_html("<b><i>{}</i></b>", photos_count)
+
+    number_of_photos.short_description = 'Total photos'
