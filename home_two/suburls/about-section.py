8e6ab67a630beed2e9c7bdb6b3c4e272caaed997
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-10 18:07:01 +0300

    - Map api view 'RetrieveAboutSectionAPIView' to a 'URLConf'.

--- home_two/suburls/about-section.py
+++ home_two/suburls/about-section.py
@@ -1,3 +1,4 @@
+from home_two.views.about_section import RetrieveAboutSectionAPIView
 from custom_utils.get_file_name_util import get_file_name
 from django.urls import path
 
@@ -5,4 +6,6 @@ URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _RETRIEVE_ABOUT_SECTION_ROUTE = ''
 
-urlpatterns = []
+urlpatterns = [
+    path(_RETRIEVE_ABOUT_SECTION_ROUTE, RetrieveAboutSectionAPIView.as_view()),
+]
