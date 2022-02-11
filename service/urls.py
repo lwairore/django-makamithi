1306de7fbf4522091e19d50f010fa1360e3ff2a9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 09:21:02 +0300

    - Update how set the value of file location.

--- service/urls.py
+++ service/urls.py
@@ -5,16 +5,14 @@ from django.urls.conf import path, include
 
 app_name = ServiceConfig.name
 
-_BASE_INCLUDE_PATH = '{app_name}.{URL_FILE_NAME}.{FILE_NAME_WITHOUT_EXTENSION}'\
-    .format(
-        app_name=app_name,
-        URL_FILE_NAME=URL_FILE_NAME)
+_BASE_INCLUDE_PATH = '{}.{}.'.format(
+    app_name,
+    URL_FILE_NAME)
 
 urlpatterns = [
     # Routes for showing list of services
     path(LIST_SERVICE_ROOT_ROUTE,
          include(
-             _BASE_INCLUDE_PATH.format(
-                 FILE_NAME_WITHOUT_EXTENSION=URL_FILE_NAME_WITHOUT_EXTENSION)
+             _BASE_INCLUDE_PATH + URL_FILE_NAME_WITHOUT_EXTENSION
          ))
 ]
