72c21ec721bea0074f534aee22c50f07d70ead53
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 09:34:31 +0300

    - Add attributes 'date_hierarchy' and 'list_filter'.

--- service/subadmins/service.py
+++ service/subadmins/service.py
@@ -7,3 +7,5 @@ class ServiceModelAdmin(ModelAdmin):
     list_display = ('title', 'photo',)
     search_fields = ('title', 'summary',)
     raw_id_fields = ('photo',)
+    date_hierarchy = 'modified_date'
+    list_filter = ('created_at', 'modified_date',)
