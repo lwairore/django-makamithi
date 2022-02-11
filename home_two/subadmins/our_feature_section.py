3d5bf7775fa6f11d486fcd79da262c27d423a2cd
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 13:38:36 +0300

    - Create model admin 'FeatureSectionModelAdmin'.

---
+++ home_two/subadmins/our_feature_section.py
@@ -0,0 +1,11 @@
+from home_two.submodels.our_feature_section import FeatureSectionModel
+from django.contrib.admin import ModelAdmin, register
+
+
+@register(FeatureSectionModel)
+class FeatureSectionModelAdmin(ModelAdmin):
+    list_display = ('heading', 'photo',)
+    search_fields = ('heading', 'description',)
+    date_hierarchy = 'created_at'
+    list_filter = ('created_at', 'modified_date',)
+    raw_id_fields = ('photo',)
