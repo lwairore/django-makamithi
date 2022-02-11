953e6d3b57802bd7c979c7cbf56711f4cbfa8163
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 13:36:01 +0300

    - Create model FeatureSectionModel

---
+++ home_two/submodels/our_feature_section.py
@@ -0,0 +1,19 @@
+from django.db.models import Model
+from django.db.models.deletion import PROTECT
+from django.db.models.fields import CharField, DateTimeField, TextField
+from django.db.models.fields.related import ForeignKey
+
+
+class FeatureSectionModel(Model):
+    heading = CharField(max_length=10)
+    description = TextField(max_length=250, blank=True, null=True)
+    photo = ForeignKey('PhotoModel', blank=True, null=True, on_delete=PROTECT)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.heading
+
+    class Meta:
+        verbose_name = 'Feature section'
+        verbose_name_plural = 'Feature sections'
