270c260d27b62b74e6e34c5be9730f1f9b2c690e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 20:19:53 +0300

    - Define method '_get_product_category_instance'.

--- shop/views/product.py
+++ shop/views/product.py
@@ -1,6 +1,18 @@
+from shop.submodels.product_category import ProductCategoryModel
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 
 class ListProductAPIView(APIView):
     permission_classes = (AllowAny,)
+
+    def _get_product_category_instance(self, product_category_id: int):
+        product_category_instance = None
+
+        try:
+            product_category_instance = ProductCategoryModel.objects\
+                .only('id').get(id=product_category_id)
+        except ProductCategoryModel.DoesNotExist as product_category_does_not_exist:
+            raise product_category_does_not_exist
+
+        return product_category_instance
