ab357286403b3a3523436b231f9d6c68d18b4dbb
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 13:49:29 +0300

    - Map api view 'RetrieveFeatureSectionAPIView' to a 'URLConf'.

--- home_two/suburls/our_feature_section.py
+++ home_two/suburls/our_feature_section.py
@@ -1,3 +1,5 @@
+from home_two.views.our_feature_section import RetrieveFeatureSectionAPIView
+from django.urls import path
 from custom_utils.get_file_name_util import get_file_name
 
 
@@ -6,3 +8,8 @@ FEATURE_SECTION_ROOT_ROUTE = 'retrieve/feature-section'
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _RETRIEVE_FEATURE_SECTION_URL_ROUTE = ''
+
+urlpatterns = [
+    path(_RETRIEVE_FEATURE_SECTION_URL_ROUTE,
+         RetrieveFeatureSectionAPIView.as_view()),
+]
