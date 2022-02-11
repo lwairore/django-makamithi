6155f04d7cc12771e866fe949816bd033a050024
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 17:10:23 +0300

    - Define method '_list_product_category_kueryset' on 'ListProductCategoryAPIView'.

--- shop/views/product_category.py
+++ shop/views/product_category.py
@@ -1,3 +1,4 @@
+from shop.submodels.product_category import ProductCategoryModel
 from shop.serializers.product_category import RetrieveProductCategorySerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
@@ -6,3 +7,10 @@ from rest_framework.views import APIView
 class ListProductCategoryAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveProductCategorySerializer
+
+    def _list_product_category_kueryset(self):
+        product_category_kueryset = ProductCategoryModel.objects\
+            .only('title', 'flaticon', 'description', 'id',).order_by()\
+            .all()
+
+        return product_category_kueryset
