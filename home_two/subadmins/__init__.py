e5adeac8d2ea552ed55fd5916929ff57d6b98ab7
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 13:39:01 +0300

    - Import model admin 'FeatureSectionModelAdmin'.

--- home_two/subadmins/__init__.py
+++ home_two/subadmins/__init__.py
@@ -1,3 +1,4 @@
 from .about_section import AboutSectionModelAdmin
 from .banner_ad import BannerAdModelAdmin
 from .preview_item import PhotoModelAdmin
+from .our_feature_section import FeatureSectionModelAdmin
