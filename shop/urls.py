0c28b4e3012790760925c6b0c8e678be4d5d1998
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 17:19:23 +0300

    - Include all 'URLConfs' for package 'product_category'.

--- shop/urls.py
+++ shop/urls.py
@@ -1,3 +1,6 @@
+from shop.suburls.product_category import (PRODUCT_CATEGORY_ROOT_ROUTE,
+                                           URL_FILE_NAME_WITHOUT_EXTENSION as PRODUCT_CATEGORY_URL_FILE_NAME_WITHOUT_EXTENSION)
+from django.urls import path, include
 from shop.suburls import URL_FILE_NAME
 from shop.apps import ShopConfig
 
@@ -8,4 +11,9 @@ _BASE_INCLUDE_PATH = '{}.{}.'.format(
     app_name,
     URL_FILE_NAME)
 
-urlpatterns = []
+urlpatterns = [
+    # Routes for product category
+    path(PRODUCT_CATEGORY_ROOT_ROUTE,
+         include(
+             _BASE_INCLUDE_PATH + PRODUCT_CATEGORY_URL_FILE_NAME_WITHOUT_EXTENSION))
+]
