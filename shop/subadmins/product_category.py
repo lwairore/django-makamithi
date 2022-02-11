93e0decb1b8c704c91b2de4f2bd205f99b6e4326
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 20:49:15 +0300

    - Add and configure attribute 'search_fields'.

--- shop/subadmins/product_category.py
+++ shop/subadmins/product_category.py
@@ -7,3 +7,4 @@ class ProductCategoryModelAdmin(ModelAdmin):
     list_display = ('title', 'flaticon',)
     date_hierarchy = 'created_at'
     list_filter = ('created_at', 'modified_date',)
+    search_fields = ('title', 'description',)
