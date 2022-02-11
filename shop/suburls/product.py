ba97fd6c2482a1c21d95720e00077d4313bbb7ff
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 20:32:38 +0300

    - Initialize constant 'PRODUCT_ROOT_ROUTE'.

--- shop/suburls/product.py
+++ shop/suburls/product.py
@@ -1,6 +1,10 @@
+from shop.suburls.product_category import PRODUCT_CATEGORY_ROOT_ROUTE
 from custom_utils.get_file_name_util import get_file_name
 
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _LIST_PRODUCT_ROUTE = 'list/'
+
+PRODUCT_ROOT_ROUTE = PRODUCT_CATEGORY_ROOT_ROUTE + \
+    '<int:product_category_id>/products'
