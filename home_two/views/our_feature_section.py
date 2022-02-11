13f4efb2599dce2b7b8253a4c37e8b2c4ede13e8
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 13:44:46 +0300

    - Define method '_get_feature_section_instance'.

--- home_two/views/our_feature_section.py
+++ home_two/views/our_feature_section.py
@@ -1,3 +1,4 @@
+from home_two.submodels.our_feature_section import FeatureSectionModel
 from home_two.serializers.our_feature_section import RetrieveFeatureSectionSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
@@ -6,4 +7,12 @@ from rest_framework.views import APIView
 class RetrieveFeatureSectionAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveFeatureSectionSerializer
-    
+
+    def _get_feature_section_instance(self):
+        feature_section_instance = FeatureSectionModel.objects\
+            .select_related('photo')\
+            .order_by()\
+            .only('heading', 'description', 'photo__image',
+                  'photo__caption').first()
+
+        return feature_section_instance
