fb789611c49d44a552e814eeb73db01435bece8c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 09:27:22 +0300

    - Add attribute 'search_fields'.

--- service/subadmins/service.py
+++ service/subadmins/service.py
@@ -5,4 +5,5 @@ from django.contrib.admin import ModelAdmin, register
 @register(ServiceModel)
 class ServiceModelAdmin(ModelAdmin):
     list_display = ('title', 'photo',)
+    search_fields = ('title', 'summary',)
     raw_id_fields = ('photo',)
