ca24d03bfb4061c2ef92ce4dbca116a790281fd5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 20:57:05 +0300

    - Alter field category on productmodel. - Remove field category from productmodel - Add field category to productmodel

--- shop/subadmins/product.py
+++ shop/subadmins/product.py
@@ -4,8 +4,8 @@ from django.contrib.admin import ModelAdmin, register
 
 @register(ProductModel)
 class ProductModelAdmin(ModelAdmin):
-    list_display = ('title', 'category', 'photo',)
-    raw_id_fields = ('category', 'photo',)
-    filter_horizontal = ('price',)
+    list_display = ('title', 'photo',)
+    raw_id_fields = ('photo',)
+    filter_horizontal = ('price', 'category', )
     list_filter = ('created_at', 'modified_date',)
     date_hierarchy = 'created_at'
--- shop/submodels/product.py
+++ shop/submodels/product.py
@@ -6,8 +6,8 @@ from django.db.models.fields import DateTimeField
 
 class ProductModel(Model):
     title = CharField(max_length=250)
-    category = ForeignKey('ProductCategoryModel', on_delete=PROTECT,
-                          blank=True, null=True, related_name='product_category')
+    category = ManyToManyField('ProductCategoryModel',
+                               blank=True)
     price = ManyToManyField('PriceModel', blank=True)
     created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
     modified_date = DateTimeField(auto_now=True, blank=True, null=True)
