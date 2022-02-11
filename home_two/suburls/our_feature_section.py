df60b6ad373c212f15bebfa9e1049a981d5aeb02
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 13:48:39 +0300

    - Initialize constant '_RETRIEVE_FEATURE_SECTION_URL_ROUTE'.

--- home_two/suburls/our_feature_section.py
+++ home_two/suburls/our_feature_section.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 FEATURE_SECTION_ROOT_ROUTE = 'retrieve/feature-section'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_RETRIEVE_FEATURE_SECTION_URL_ROUTE = ''
