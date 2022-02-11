faa5d28b25a67aa2c833edcb3b494d38f333efab
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 17:17:04 +0300

    - Initialize constant '_BASE_INCLUDE_PATH'.

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,11 @@
+from shop.suburls import URL_FILE_NAME
 from shop.apps import ShopConfig
 
 
 app_name = ShopConfig.name
+
+_BASE_INCLUDE_PATH = '{}.{}.'.format(
+    app_name,
+    URL_FILE_NAME)
 
 urlpatterns = []
