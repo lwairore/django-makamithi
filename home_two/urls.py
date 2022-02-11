3b114c362d63ae0aa337f42c842324e716bd007c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 14:00:45 +0300

    - Include all routes for feature package.

--- home_two/urls.py
+++ home_two/urls.py
@@ -1,3 +1,4 @@
+from home_two.suburls.our_feature_section import FEATURE_SECTION_ROOT_ROUTE, URL_FILE_NAME_WITHOUT_EXTENSION as FEATURE_FILE_NAME_WITHOUT_EXTENSION
 from home_two.suburls.about_section import ABOUT_SECTION_ROOT_ROUTE, URL_FILE_NAME_WITHOUT_EXTENSION as ABOUT_SECTION_FILE_NAME_WITHOUT_EXTENSION
 from home_two.suburls.banner_ad import BANNER_AD_ROOT_ROUTE, URL_FILE_NAME_WITHOUT_EXTENSION as BANNER_AD_FILE_NAME_WITHOUT_EXTENSION
 from home_two.apps import HomeTwoConfig
@@ -20,4 +21,8 @@ urlpatterns = [
     #  Routes for About section
     path(ABOUT_SECTION_ROOT_ROUTE,
          _BASE_INCLUDE_PATH + ABOUT_SECTION_FILE_NAME_WITHOUT_EXTENSION),
+
+    # Routes for Feature section
+    path(FEATURE_SECTION_ROOT_ROUTE,
+         _BASE_INCLUDE_PATH + FEATURE_FILE_NAME_WITHOUT_EXTENSION),
 ]
