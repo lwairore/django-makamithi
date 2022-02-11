b453d3ee76407fc9aadb9eba57e1782f0045dc29
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 17:07:52 +0300

    - Add private attribute '_serializer_class'.

--- shop/views/product_category.py
+++ shop/views/product_category.py
@@ -1,6 +1,8 @@
+from shop.serializers.product_category import RetrieveProductCategorySerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 
 class ListProductCategoryAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveProductCategorySerializer
