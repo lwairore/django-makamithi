efdeb73542fe5a113a3cd75daa9435f56b87abb1
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 17:20:18 +0300

    - Update constant '_LIST_PRODUCT_CATEGORY_ROUTE'. - Update constant 'PRODUCT_CATEGORY_ROOT_ROUTE'.

--- shop/suburls/product_category.py
+++ shop/suburls/product_category.py
@@ -5,9 +5,9 @@ from custom_utils.get_file_name_util import get_file_name
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
-_LIST_PRODUCT_CATEGORY_ROUTE = ''
+_LIST_PRODUCT_CATEGORY_ROUTE = 'list/'
 
-PRODUCT_CATEGORY_ROOT_ROUTE = 'list/product/category/'
+PRODUCT_CATEGORY_ROOT_ROUTE = 'product/category/'
 
 
 urlpatterns = [
