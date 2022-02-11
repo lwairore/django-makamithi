f3796b517e93f2ade904793d6cdd8e7239fcacfa
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 13:43:48 +0300

    - Add private attribute '_serializer_class'.

--- home_two/views/our_feature_section.py
+++ home_two/views/our_feature_section.py
@@ -1,6 +1,9 @@
+from home_two.serializers.our_feature_section import RetrieveFeatureSectionSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 
 class RetrieveFeatureSectionAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveFeatureSectionSerializer
+    
