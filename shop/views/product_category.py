5d17830d3ede29de56f41524acb1638317ab542e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 17:11:45 +0300

    - Define method 'get' on 'ListProductCategoryAPIView'.

--- shop/views/product_category.py
+++ shop/views/product_category.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from shop.submodels.product_category import ProductCategoryModel
 from shop.serializers.product_category import RetrieveProductCategorySerializer
 from rest_framework.permissions import AllowAny
@@ -14,3 +15,11 @@ class ListProductCategoryAPIView(APIView):
             .all()
 
         return product_category_kueryset
+
+    def get(self, request):
+        product_category_kueryset = self._list_product_category_kueryset()
+
+        product_category_kueryset_serializer = self._serializer_class(product_category_kueryset,
+                                                                      many=True)
+
+        return Response(product_category_kueryset_serializer.data)
