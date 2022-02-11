5c7f9c10fdf5673539b2859cf68edacd47740d08
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 20:36:57 +0300

    - Fix TypeError: __init__() missing 2 required positional arguments: 'max_digits' and 'decimal_places'.

--- shop/serializers/product.py
+++ shop/serializers/product.py
@@ -4,7 +4,8 @@ from rest_framework.serializers import ModelSerializer
 
 
 class RetrieveProductSerializer(ModelSerializer):
-    price = DecimalField(source='price.now', required=False)
+    price = DecimalField(source='price.now', required=False, max_digits=10,
+                         decimal_places=2)
 
     class Meta:
         model = ProductModel
