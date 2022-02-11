716d4ddd02e46dd7094f9614ddfb8eee070f9524
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 08:59:06 +0300

    - Define method '_list_service'.

--- service/views/service.py
+++ service/views/service.py
@@ -1,8 +1,18 @@
+from service.submodels.service import ServiceModel
 from service.serializers.service import RetrieveServiceModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
+
 class ListServiceAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveServiceModelSerializer
-    
+
+    def _list_service(self):
+        service_kueryset = ServiceModel.objects\
+            .select_related('photo')\
+            .order_by()\
+            .only('photo__image', 'photo__caption', 'id', 'title', 'summary',)\
+            .all()
+
+        return service_kueryset
