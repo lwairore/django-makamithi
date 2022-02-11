d1e80d58bbc27a8192f2baf8634d02c7655589ad
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 17:37:28 +0300

    - Create model admin 'ProductCategoryModelAdmin'.

---
+++ shop/subadmins/product_category.py
@@ -0,0 +1,9 @@
+from shop.submodels.product_category import ProductCategoryModel
+from django.contrib.admin import ModelAdmin, register
+
+
+@register(ProductCategoryModel)
+class ProductCategoryModelAdmin(ModelAdmin):
+    list_display = ('title', 'flaticon',)
+    date_hierarchy = 'created_at'
+    list_filter = ('created_at', 'modified_date',)
