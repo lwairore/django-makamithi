80597ae0c8f23caa5fb02f8267644645c8de58bc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 20:29:06 +0300

    - Define method 'get' on 'ListProductAPIView'.

--- shop/views/product.py
+++ shop/views/product.py
@@ -1,12 +1,16 @@
+from shop.serializers.product import RetrieveProductSerializer
+from rest_framework.response import Response
 from django.db.models.query import QuerySet
 from shop.submodels.product import ProductModel
 from shop.submodels.product_category import ProductCategoryModel
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
+from rest_framework.status import HTTP_404_NOT_FOUND
 
 
 class ListProductAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveProductSerializer
 
     def _get_product_category_instance(self, product_category_id: int):
         product_category_instance = None
@@ -25,3 +29,21 @@ class ListProductAPIView(APIView):
             .order_by().all()
 
         return product_kueryset
+
+    def get(self, request, product_category_id: int):
+        try:
+            self._get_product_category_instance(
+                product_category_id=product_category_id)
+        except ProductCategoryModel.DoesNotExist:
+            return Response(
+                data={
+                    'error': f'Product category with id {product_category_id} does not exist'},
+                status=HTTP_404_NOT_FOUND)
+
+        product_kueryset = self._list_product_kueryset(
+            product_category_id=product_category_id)
+
+        product_kueryset_serializer = self._serializer_class(
+            product_kueryset, many=True)
+
+        return Response(product_kueryset_serializer.data)
