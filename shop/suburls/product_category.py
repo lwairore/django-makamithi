5bd7470014dbf0b15b4013e9c1d8894a0e8f1d9c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 17:15:44 +0300

    - Map api view 'ListProductCategoryAPIView' to a 'URLConf'.

--- shop/suburls/product_category.py
+++ shop/suburls/product_category.py
@@ -1,3 +1,5 @@
+from shop.views.product_category import ListProductCategoryAPIView
+from django.urls import path
 from custom_utils.get_file_name_util import get_file_name
 
 
@@ -6,3 +8,8 @@ URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 _LIST_PRODUCT_CATEGORY_ROUTE = ''
 
 PRODUCT_CATEGORY_ROOT_ROUTE = 'list/product/category/'
+
+
+urlpatterns = [
+    path(_LIST_PRODUCT_CATEGORY_ROUTE, ListProductCategoryAPIView.as_view()),
+]
