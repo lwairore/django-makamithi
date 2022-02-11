b298c8b47742f5eb4ca094db91905c471156fdd3
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-11 13:42:51 +0300

    - Create serializer '_RetrievePhotoModelSerializer'.

--- home_two/serializers/our_feature_section.py
+++ home_two/serializers/our_feature_section.py
@@ -1,5 +1,12 @@
 from home_two.submodels.our_feature_section import FeatureSectionModel
+from home_two.submodels.preview_item import PhotoModel
 from rest_framework.serializers import ModelSerializer
+
+
+class _RetrievePhotoModelSerializer(ModelSerializer):
+    class Meta:
+        model = PhotoModel
+        fields = ('image', 'caption',)
 
 
 class RetrieveFeatureSectionSerializer(ModelSerializer):
