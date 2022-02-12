00af916d8bc58ac4ad5c6ded08a04a87e6752371
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 19:46:12 +0300

    - Create model admin 'SeoSocialShareDataModelAdmin'.

---
+++ social_sharing/subadmins/seo_social_share_data.py
@@ -0,0 +1,10 @@
+from django.contrib.admin import ModelAdmin
+
+
+class SeoSocialShareDataModelAdmin(ModelAdmin):
+    list_display = ('title', 'keywords', 'url', 'type',
+                    'author', 'section', 'published', 'modified',)
+    search_fields = ('title', 'keywords', 'description',
+                     'url', 'type', 'author', 'section')
+    raw_id_fields = ('image',)
+    list_filter = ('modified', 'published',)
