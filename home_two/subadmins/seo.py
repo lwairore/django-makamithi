c94ff5f80abcf4a268ab4002d2121a9500aafd35
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 19:47:27 +0300

    - Create model admin 'HomeSEODetailsModelAdmin'.

---
+++ home_two/subadmins/seo.py
@@ -0,0 +1,15 @@
+from social_sharing.subadmins.seo_social_share_data import SeoSocialShareDataModelAdmin
+from home_two.submodels.seo import HomeSEODetailsModel
+from django.contrib.admin import register
+
+
+@register(HomeSEODetailsModel)
+class HomeSEODetailsModelAdmin(SeoSocialShareDataModelAdmin):
+    def has_add_permission(self, request):
+        # check if generally has add permission
+        should_add_instance = super().has_add_permission(request)
+        # set add permission to False, if object already exists
+        if should_add_instance and HomeSEODetailsModel.objects.only('pk').exists():
+            should_add_instance = False
+
+        return should_add_instance
