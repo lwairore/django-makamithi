17ec19a090e12feb959830b53a57b094551a27ea
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 13:30:17 +0300

    - Create model VisitNowCtaSectionModel.

--- home_two/submodels/__init__.py
+++ home_two/submodels/__init__.py
@@ -2,3 +2,4 @@ from .preview_item import PhotoModel
 from .banner_ad import BannerAdModel
 from .about_section import AboutSectionModel
 from .our_feature_section import FeatureSectionModel
+from .visit_now_cta_section import VisitNowCtaSectionModel
---
+++ home_two/submodels/visit_now_cta_section.py
@@ -0,0 +1,24 @@
+from django.db.models.base import Model
+from django.db.models.deletion import PROTECT
+from django.db.models.fields import CharField, DateTimeField, TextField
+from django.db.models.fields.related import ForeignKey
+
+
+class VisitNowCtaSectionModel(Model):
+    heading = CharField(max_length=60)
+    description = TextField(max_length=150, blank=True, null=True)
+    background_image = ForeignKey(
+        'PhotoModel', blank=True, null=True, on_delete=PROTECT,
+        related_name='visit_now_cta_section_background_image')
+    section_image = ForeignKey(
+        'PhotoModel', blank=True, null=True, on_delete=PROTECT,
+        related_name='visit_now_cta_section_section_image')
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.heading
+
+    class Meta:
+        verbose_name = 'Visit now cta section'
+        verbose_name_plural = 'Visit now cta sections'
