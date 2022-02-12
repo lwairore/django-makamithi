4cdd917ae72bf2b440952d326b0c38b05a069f86
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 17:01:49 +0300

    - Initialize constant 'ARTICLE_VALUES'.

---
+++ social_sharing/og_type_values.py
@@ -0,0 +1,13 @@
+ARTICLE_VALUES = (
+    ('article', 'Article'),
+    ('article:published_time',
+     'Article - Published Time - When the article was first published.'),
+    ('article:modified_time',
+     'Article - Modified Time - When the article was last changed.'),
+    ('article:expiration_time',
+     'Article - Expiration Time - When the article is out of date after.'),
+    ('article:author', 'Article - Author - Writers of the article.'),
+    ('article:section',
+     'Article - Article - Section - A high-level section name. E.g. Technology'),
+    ('article:tag', 'Article - Tag -Tag words associated with this article.')
+)
