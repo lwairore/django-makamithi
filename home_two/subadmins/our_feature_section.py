757f4347adca2f470b3d981341a8be9b3dda01c0
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 15:41:22 +0300

    - Check if generally has 'add' permission. Then set 'add' permission to False, if object exists.

--- home_two/subadmins/our_feature_section.py
+++ home_two/subadmins/our_feature_section.py
@@ -9,3 +9,12 @@ class FeatureSectionModelAdmin(ModelAdmin):
     date_hierarchy = 'created_at'
     list_filter = ('created_at', 'modified_date',)
     raw_id_fields = ('background_image', 'section_image',)
+
+    def has_add_permission(self, request):
+        # check if generally has add permission
+        should_add_instance = super().has_add_permission(request)
+        # set add permission to False, if object already exists
+        if should_add_instance and FeatureSectionModel.objects.only('pk').exists():
+            should_add_instance = False
+
+        return should_add_instance
