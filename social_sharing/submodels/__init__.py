dd62b2a176d1252bd465bc5d29685a1b7d4f152d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 17:14:52 +0300

    - Create model OgTypeModel

--- social_sharing/submodels/__init__.py
+++ social_sharing/submodels/__init__.py
@@ -1,1 +1,2 @@
 from .image_aux_data import ImageAuxDataModel
+from .og_type import OgTypeModel
---
+++ social_sharing/submodels/og_type.py
@@ -0,0 +1,16 @@
+from social_sharing.og_type_values import OG_TYPE_VALUES
+from django.db.models import Model
+from django.db.models.fields import CharField, TextField
+
+
+class OgTypeModel(Model):
+    type_option = CharField(max_length=30,
+                            choices=OG_TYPE_VALUES)
+    type_value = TextField(max_length=500, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.type_option
+
+    class Meta:
+        verbose_name = 'Og type'
+        verbose_name_plural = 'Og types'
