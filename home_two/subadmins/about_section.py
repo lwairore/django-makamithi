645d9600ffefe18cd1be2d55693e9f8b2506ca65
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 15:43:17 +0300

    - Check if generally has 'add' permission. Then set 'add' permission to False, if object exists.

--- home_two/subadmins/about_section.py
+++ home_two/subadmins/about_section.py
@@ -4,5 +4,14 @@ from django.contrib.admin import ModelAdmin, register
 
 @register(AboutSectionModel)
 class AboutSectionModelAdmin(ModelAdmin):
-    list_display = ('heading', 'subheading','photo',)
+    list_display = ('heading', 'subheading', 'photo',)
     raw_id_fields = ('photo',)
+
+    def has_add_permission(self, request):
+        # check if generally has add permission
+        should_add_instance = super().has_add_permission(request)
+        # set add permission to False, if object already exists
+        if should_add_instance and AboutSectionModel.objects.only('pk').exists():
+            should_add_instance = False
+
+        return should_add_instance
