9956123146b36f17bae2f0159830e58c4de2bd7a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 10:50:44 +0300

    - Overwrite field 'photo'.

--- shop/serializers/product.py
+++ shop/serializers/product.py
@@ -11,6 +11,7 @@ class _RetrievePhotoModelSerializer(ModelSerializer):
 
 
 class RetrieveProductSerializer(ModelSerializer):
+    photo = _RetrievePhotoModelSerializer(required=False)
     price = DecimalField(source='price.now', required=False, max_digits=10,
                          decimal_places=2)
 
