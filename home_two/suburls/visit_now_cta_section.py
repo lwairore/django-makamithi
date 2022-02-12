3d52dba365e4dbe1a25500225e6fb9da7c2cfd77
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 13:49:07 +0300

    - Initialize constant 'URL_FILE_NAME_WITHOUT_EXTENSION'.

--- home_two/suburls/visit_now_cta_section.py
+++ home_two/suburls/visit_now_cta_section.py
@@ -1,1 +1,5 @@
+from custom_utils.get_file_name_util import get_file_name
+
 VISIT_NOW_CTA_SECTION_ROOT_ROUTE = 'retrieve/visit-now-cta-section/'
+
+URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
