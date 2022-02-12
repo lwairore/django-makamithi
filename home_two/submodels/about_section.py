1d13e5d8a0851c96a0054db9508d6d1d5effcad3
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 15:43:39 +0300

    - Overwrite methods 'clean' and 'save'.

--- home_two/submodels/about_section.py
+++ home_two/submodels/about_section.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db.models import Model
 from django.db.models.deletion import PROTECT
 from django.db.models.fields import CharField, TextField
@@ -12,6 +13,18 @@ class AboutSectionModel(Model):
 
     def __str__(self) -> str:
         return self.heading
+
+    def clean(self):
+        if AboutSectionModel.objects.exists() and not self.pk:
+            raise ValidationError("You can only have one company")
+
+    def save(self, *args, **kwargs):
+        if not self.pk and AboutSectionModel.objects.exists():
+            # if you'll not check for self.pk
+            # then error will also raised in update of exists model
+            raise ValidationError(
+                'There is can be only one "AboutSectionModel" instance')
+        return super(AboutSectionModel, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'About section'
