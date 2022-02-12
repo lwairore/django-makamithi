e8f1a894261261dd712afc19121626b06493b8c4
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 14:40:08 +0300

    - Create and configure attribute '_serializer_class'.

--- home_two/views/why_choose_us_section.py
+++ home_two/views/why_choose_us_section.py
@@ -1,6 +1,8 @@
+from home_two.serializers.why_choose_us_section import RetrieveWhyChooseUsSectionSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 
-class RetrievewhyChooseUsSectionAPIView(APIView):
+class RetrieveWhyChooseUsSectionAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveWhyChooseUsSectionSerializer
