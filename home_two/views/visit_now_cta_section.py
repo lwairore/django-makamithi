d5a432f222b511c5f1f26a7d57031fd8c02d35ec
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 13:41:25 +0300

    - Define method '_get_visit_now_cta_section_instance'.

--- home_two/views/visit_now_cta_section.py
+++ home_two/views/visit_now_cta_section.py
@@ -1,6 +1,18 @@
+from home_two.submodels.visit_now_cta_section import VisitNowCtaSectionModel
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 
 class RetrieveVisitNowCtaSectionAPIView(APIView):
     permission_classes = (AllowAny,)
+
+    def _get_visit_now_cta_section_instance(self):
+        visit_now_cta_section_instance = VisitNowCtaSectionModel.objects\
+            .select_related('background_image', 'section_image',)\
+            .order_by()\
+            .only('heading', 'description', 'section_image__image',
+                  'section_image__caption',
+                  'background_image__image',
+                  'background_image__caption').first()
+
+        return visit_now_cta_section_instance
