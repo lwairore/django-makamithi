b00bb0700c30328e1cc84e8a377d3f45173ccb68
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 17:32:24 +0300

    - Add field created_at to imageauxdatamodel

--- social_sharing/submodels/image_aux_data.py
+++ social_sharing/submodels/image_aux_data.py
@@ -1,7 +1,7 @@
 from home_two.submodels.preview_item import PhotoModel
 from django.db.models import Model
 from django.db.models.deletion import PROTECT
-from django.db.models.fields import CharField, PositiveIntegerField, TextField, URLField
+from django.db.models.fields import CharField, DateTimeField, PositiveIntegerField, TextField, URLField
 from django.db.models.fields.related import ForeignKey
 
 
@@ -13,7 +13,8 @@ class ImageAuxDataModel(Model):
     alt = TextField(max_length=500, blank=True, null=True)
     image = ForeignKey(
         PhotoModel, blank=True, null=True, on_delete=PROTECT)
-
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    
     def __str__(self) -> str:
         return f'Image ID={self.id}'
 
