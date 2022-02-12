28a9235fe263bb1c6cbb90316ab372dfad576b74
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 14:37:49 +0300

    - Create private model serializer '_RetrievePhotoModelSerializer'.

--- home_two/serializers/why_choose_us_section.py
+++ home_two/serializers/why_choose_us_section.py
@@ -1,5 +1,12 @@
+from home_two.submodels.preview_item import PhotoModel
 from home_two.submodels import WhyChooseUsSectionModel
 from rest_framework.serializers import ModelSerializer
+
+
+class _RetrievePhotoModelSerializer(ModelSerializer):
+    class Meta:
+        model = PhotoModel
+        fields = ('image', 'caption',)
 
 
 class RetrieveWhyChooseUsSectionSerializer(ModelSerializer):
