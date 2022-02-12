068b3bfc1341b527f2588cd698813483e64f5308
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 17:18:05 +0300

    - Add field created_at to ogtypemodel

--- social_sharing/submodels/og_type.py
+++ social_sharing/submodels/og_type.py
@@ -1,12 +1,13 @@
 from social_sharing.og_type_values import OG_TYPE_VALUES
 from django.db.models import Model
-from django.db.models.fields import CharField, TextField
+from django.db.models.fields import CharField, DateTimeField, TextField
 
 
 class OgTypeModel(Model):
     type_option = CharField(max_length=30,
                             choices=OG_TYPE_VALUES)
     type_value = TextField(max_length=500, blank=True, null=True)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.type_option
