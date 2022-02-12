c6daaf8f3b6ffad3b41a2ea7dc21252c22d59919
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 14:32:13 +0300

    - Import model 'WhyChooseUsSectionModel'.

--- home_two/submodels/__init__.py
+++ home_two/submodels/__init__.py
@@ -3,3 +3,4 @@ from .banner_ad import BannerAdModel
 from .about_section import AboutSectionModel
 from .our_feature_section import FeatureSectionModel
 from .visit_now_cta_section import VisitNowCtaSectionModel
+from .why_choose_us_section import WhyChooseUsSectionModel
