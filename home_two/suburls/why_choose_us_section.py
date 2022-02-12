c6cb2dcb64fc8fc88d86d3a72c621cf4af9ea83d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 15:12:42 +0300

    - Initialize constant '_RETRIEVE_WHY_CHOOSE_US_SECTION_URL_ROUTE'.

--- home_two/suburls/why_choose_us_section.py
+++ home_two/suburls/why_choose_us_section.py
@@ -3,3 +3,5 @@ from custom_utils.get_file_name_util import get_file_name
 WHY_CHOOSE_US_SECTION_ROOT_ROUTE = 'retrieve/why-choose-us-section/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_RETRIEVE_WHY_CHOOSE_US_SECTION_URL_ROUTE = ''
