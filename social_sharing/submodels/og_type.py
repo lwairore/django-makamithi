f38cfed02355099a968eaeb21090f0ac3757cdae
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 17:18:26 +0300

    - Add field modified_date to ogtypemodel

--- social_sharing/submodels/og_type.py
+++ social_sharing/submodels/og_type.py
@@ -8,6 +8,7 @@ class OgTypeModel(Model):
                             choices=OG_TYPE_VALUES)
     type_value = TextField(max_length=500, blank=True, null=True)
     created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.type_option
