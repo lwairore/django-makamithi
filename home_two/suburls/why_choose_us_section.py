ab9a51c08d9fd36a527927d1d9b808ba47fb9353
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-12 15:12:20 +0300

    - Initialize constant 'URL_FILE_NAME_WITHOUT_EXTENSION'.

--- home_two/suburls/why_choose_us_section.py
+++ home_two/suburls/why_choose_us_section.py
@@ -1,1 +1,5 @@
+from custom_utils.get_file_name_util import get_file_name
+
 WHY_CHOOSE_US_SECTION_ROOT_ROUTE = 'retrieve/why-choose-us-section/'
+
+URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
