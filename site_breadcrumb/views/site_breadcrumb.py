72a0e48f0a457e01ae3ff094ca50c5f56a30c8d5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 21:05:22 +0300

    - Add a private attribute '_serializer_class'.

--- site_breadcrumb/views/site_breadcrumb.py
+++ site_breadcrumb/views/site_breadcrumb.py
@@ -1,3 +1,4 @@
+from site_breadcrumb.serializers.site_breadcrumb import RetrieveSiteBreadcrumbModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,3 +6,4 @@ from rest_framework.views import APIView
 
 class RetrieveSiteBreadcrumbModelAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveSiteBreadcrumbModelSerializer
