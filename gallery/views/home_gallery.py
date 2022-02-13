92f1bd2c6fe623620a81f3b4c74e223ae90eb461
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 16:21:05 +0300

    - Define method '_list_gallery_kueryset'.

--- gallery/views/home_gallery.py
+++ gallery/views/home_gallery.py
@@ -1,3 +1,4 @@
+from gallery.submodels.gallery import GalleryModel
 from gallery.serializers.home_gallery import ListHomeGallerySerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -7,3 +8,11 @@ from rest_framework.views import APIView
 class ListHomeGalleryAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = ListHomeGallerySerializer
+
+    def _list_gallery_kueryset(self):
+        gallery_kueryset = GalleryModel.objects\
+            .only('image__image', 'image__caption',)\
+            .select_related('image')\
+            .order_by().all()
+
+        return gallery_kueryset
