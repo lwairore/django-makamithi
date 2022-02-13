71ccf2014b0dbb1af9269f1de9d31729f4227e90
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 10:40:19 +0300

    - Reassign the value for constant 'WHY_CHOOSE_US_SECTION_ROOT_ROUTE'

--- home_two/suburls/why_choose_us_section.py
+++ home_two/suburls/why_choose_us_section.py
@@ -2,7 +2,7 @@ from home_two.views.why_choose_us_section import RetrieveWhyChooseUsSectionAPIVi
 from custom_utils.get_file_name_util import get_file_name
 from django.urls import path
 
-WHY_CHOOSE_US_SECTION_ROOT_ROUTE = 'retrieve/why-choose-us-section/'
+WHY_CHOOSE_US_SECTION_ROOT_ROUTE = 'retrieve/section/why-choose-us/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
