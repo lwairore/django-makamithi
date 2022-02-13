5fe6838ce3452e48abed81f63121ee79682049df
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 13:33:50 +0300

    - Add private attribute '_serializer_class'.

--- home_two/views/core_value.py
+++ home_two/views/core_value.py
@@ -1,6 +1,8 @@
+from home_two.serializers.core_value import CoreValueModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 
 class CoreValueAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = CoreValueModelSerializer
