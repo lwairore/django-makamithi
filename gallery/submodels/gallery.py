8bfc948fa0a268eb441d69d8fc5041a46a60d76e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 16:05:33 +0300

    - Add field created_at to gallerymodel.

--- gallery/submodels/gallery.py
+++ gallery/submodels/gallery.py
@@ -1,3 +1,4 @@
+from django.db.models.fields import DateTimeField
 from home_two.submodels import PhotoModel
 from django.db.models import Model
 from django.db.models.deletion import PROTECT
@@ -7,6 +8,7 @@ from django.db.models.fields.related import ForeignKey
 class GalleryModel(Model):
     image = ForeignKey(
         PhotoModel, blank=True, null=True, on_delete=PROTECT)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'Gallery ID={self.id}'
