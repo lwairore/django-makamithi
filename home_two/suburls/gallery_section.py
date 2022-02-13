1b5b909e4737ef2f42332ecad992b56d7b288c51
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 14:44:26 +0300

    - Initialize constant '_RETRIEVE_GALLERY_SECTION_URL_ROUTE'.

--- home_two/suburls/gallery_section.py
+++ home_two/suburls/gallery_section.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 GALLERY_SECTION_ROOT_ROUTE = 'retrieve/section/gallery/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_RETRIEVE_GALLERY_SECTION_URL_ROUTE = ''
