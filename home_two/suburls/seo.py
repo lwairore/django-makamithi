a22e523fdd9cd1e93f5750b02121a006242ac316
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 10:35:50 +0300

    - Map api view 'RetrieveHomeSEODetailsAPIView' to a 'URLConf'.

--- home_two/suburls/seo.py
+++ home_two/suburls/seo.py
@@ -1,3 +1,5 @@
+from home_two.views.seo import RetrieveHomeSEODetailsAPIView
+from django.urls.conf import path
 from custom_utils.get_file_name_util import get_file_name
 
 
@@ -7,4 +9,7 @@ URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _RETRIEVE_HOME_SEO_DETAILS_URL_ROUTE = ''
 
-urlpatterns = []
+urlpatterns = [
+    path(_RETRIEVE_HOME_SEO_DETAILS_URL_ROUTE,
+         RetrieveHomeSEODetailsAPIView.as_view())
+]
