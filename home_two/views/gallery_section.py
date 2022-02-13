6cb297c9e1ee35d847d7dee1b665d7d5c8a4650e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 14:41:46 +0300

    - Define method '_get_gallery_section_instance'.

--- home_two/views/gallery_section.py
+++ home_two/views/gallery_section.py
@@ -1,3 +1,4 @@
+from home_two.submodels.gallery_section import GallerySectionModel
 from home_two.serializers.gallery_section import RetrieveGallerySectionModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -7,3 +8,13 @@ from rest_framework.views import APIView
 class RetrieveGallerySectionAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveGallerySectionModelSerializer
+
+    def _get_gallery_section_instance(self):
+        gallery_section_instance = GallerySectionModel.objects\
+            .select_related('section_image')\
+            .order_by()\
+            .only('heading', 'summary', 'section_image__image',
+                  'section_image__caption',)\
+            .first()
+
+        return gallery_section_instance
