daaaf42c45bba0f2c60d01454c4bd348bee1c87a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 16:10:36 +0300

    - Overwrite field 'image'.

--- gallery/serializers/home_gallery.py
+++ gallery/serializers/home_gallery.py
@@ -2,12 +2,16 @@ from home_two.submodels.preview_item import PhotoModel
 from gallery.submodels.gallery import GalleryModel
 from rest_framework.serializers import ModelSerializer
 
+
 class _RetrievePhotoModelSerializer(ModelSerializer):
     class Meta:
         model = PhotoModel
         fields = ('image', 'caption',)
 
+
 class ListHomeGallerySerializer(ModelSerializer):
+    image = _RetrievePhotoModelSerializer(required=False)
+
     class Meta:
         model = GalleryModel
         fields = ('image',)
