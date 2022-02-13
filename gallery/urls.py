b8a49caef36a5370e4aefd95c6f5410355753f7c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 16:46:10 +0300

    - Initialize constant '_BASE_INCLUDE_PATH'.

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,12 @@
+from gallery.suburls import URL_FILE_NAME
 from gallery.apps import GalleryConfig
 from gallery.suburls.home_gallery import HOME_GALLERY_ROOT_ROUTE
 from django.urls.conf import path, include
 
 app_name = GalleryConfig.name
+
+_BASE_INCLUDE_PATH = '{}.{}.'.format(
+    app_name,
+    URL_FILE_NAME)
 
 urlpatterns = []
