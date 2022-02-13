31e499a2f4e7c56ab57db83eaa6f9ab98c8d4bda
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 09:59:19 +0300

    - Initialize constant '_RETRIEVE_HOME_SEO_DETAILS_URL_ROUTE'.

--- home_two/suburls/seo.py
+++ home_two/suburls/seo.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 HOME_SEO_DETAILS_ROOT_ROUTE = 'retrieve/seo/home/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_RETRIEVE_HOME_SEO_DETAILS_URL_ROUTE = ''
