e21be29ead8573eee5e9e0b03bf7df53427d5de5
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 14:45:21 +0300

    - Map api view 'RetrieveGallerySectionAPIView' to a 'URLConf'.

--- home_two/suburls/gallery_section.py
+++ home_two/suburls/gallery_section.py
@@ -1,3 +1,5 @@
+from home_two.views.gallery_section import RetrieveGallerySectionAPIView
+from django.urls.conf import path
 from custom_utils.get_file_name_util import get_file_name
 
 
@@ -7,4 +9,7 @@ URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _RETRIEVE_GALLERY_SECTION_URL_ROUTE = ''
 
-urlpatterns = []
+urlpatterns = [
+    path(_RETRIEVE_GALLERY_SECTION_URL_ROUTE,
+         RetrieveGallerySectionAPIView.as_view()),
+]
