840a30ab33511caf3d64bfcb58ef5bc3fd6590f6
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 13:31:25 +0300

    - Create model serializer '_RetrievePhotoModelSerializer'.

--- home_two/serializers/core_value.py
+++ home_two/serializers/core_value.py
@@ -1,5 +1,11 @@
-from home_two.submodels.core_value import CoreValueModel
+from home_two.submodels import (PhotoModel, CoreValueModel)
 from rest_framework.serializers import ModelSerializer
+
+
+class _RetrievePhotoModelSerializer(ModelSerializer):
+    class Meta:
+        model = PhotoModel
+        fields = ('image', 'caption',)
 
 
 class CoreValueModelSerializer(ModelSerializer):
