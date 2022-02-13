9c7e3da9baf9285685a17e6d6198d01179afd3fa
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 13:38:26 +0300

    - Initialize constant '_LIST_CORE_VALUE_URL_ROUTE'.

--- home_two/suburls/core_value.py
+++ home_two/suburls/core_value.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 CORE_VALUE_ROOT_ROUTE = 'list/core-value/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_LIST_CORE_VALUE_URL_ROUTE = ''
