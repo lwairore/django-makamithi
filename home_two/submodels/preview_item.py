fa738f6f900be0053f989ac17a8ee26418fdc8f7
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 09:41:44 +0300

    - Add field height to photomodel     - Add field width to photomodel

--- home_two/submodels/preview_item.py
+++ home_two/submodels/preview_item.py
@@ -1,10 +1,12 @@
 from django.db.models.fields.files import ImageField
 from custom_utils.rename_image_filename_util import rename_uploaded_image_and_path
 from django.db.models import Model
-from django.db.models.fields import DateTimeField, TextField
+from django.db.models.fields import DateTimeField, PositiveIntegerField, TextField
 
 
 class PhotoModel(Model):
+    width = PositiveIntegerField(blank=True, null=True)
+    height = PositiveIntegerField(blank=True, null=True)
     image = ImageField(upload_to=rename_uploaded_image_and_path,
                        max_length=2000)
     caption = TextField(max_length=5000, blank=True, null=True)
