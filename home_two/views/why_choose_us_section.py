bae26c7934c61dbd7b482b7d296b6801d6eea9db
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 14:49:55 +0300

    - Fix importation of 'WhyChooseUsSectionModel'.

--- home_two/views/why_choose_us_section.py
+++ home_two/views/why_choose_us_section.py
@@ -1,5 +1,5 @@
+from home_two.submodels.why_choose_us_section import WhyChooseUsSectionModel
 from rest_framework.response import Response
-from home_two.submodels import why_choose_us_section
 from home_two.serializers.why_choose_us_section import RetrieveWhyChooseUsSectionSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
@@ -10,7 +10,7 @@ class RetrieveWhyChooseUsSectionAPIView(APIView):
     _serializer_class = RetrieveWhyChooseUsSectionSerializer
 
     def _get_why_choose_us_section_instance(self):
-        why_choose_us_section_instance = why_choose_us_section.WhyChooseUsSectionModel.objects\
+        why_choose_us_section_instance = WhyChooseUsSectionModel.objects\
             .select_related('section_image',)\
             .order_by()\
             .only('heading', 'description', 'section_image__image',
