b59d3a762f7872a27af2fa7019603f7c2b7baa30
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 09:52:42 +0300

    - Create private attribute '_serializer_class'.

--- home_two/views/seo.py
+++ home_two/views/seo.py
@@ -1,3 +1,4 @@
+from home_two.serializers.seo import RetrieveHomeSEODetailsSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,3 +6,4 @@ from rest_framework.views import APIView
 
 class RetrieveHomeSEODetailsAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveHomeSEODetailsSerializer
