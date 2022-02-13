57f5798438b9c0ef1280064257db1a4b0d4a07b9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 14:33:09 +0300

    - Create model admin 'GallerySectionModelAdmin'.

---
+++ home_two/subadmins/gallery_section.py
@@ -0,0 +1,21 @@
+from home_two.submodels import GallerySectionModel
+from django.contrib.admin import ModelAdmin, register
+
+
+@register(GallerySectionModel)
+class GallerySectionModelAdmin(ModelAdmin):
+    search_fields = ('summary', 'heading',)
+    date_hierarchy = 'created_at'
+    raw_id_fields = ('section_image',)
+    list_display = ('heading', 'summary', 'section_image',
+                    'modified_date', 'created_at',)
+    list_filter = ('modified_date', 'created_at',)
+
+    def has_add_permission(self, request):
+        # check if generally has add permission
+        should_add_instance = super().has_add_permission(request)
+        # set add permission to False, if object already exists
+        if should_add_instance and GallerySectionModel.objects.only('pk').exists():
+            should_add_instance = False
+
+        return should_add_instance
