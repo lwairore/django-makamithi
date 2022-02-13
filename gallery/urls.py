7008096ecc8f1db1bea389b07a4d8d2a1ed530a7
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 17:46:58 +0300

    - Include all url confs for 'Home Gallery'.

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
 from gallery.suburls import URL_FILE_NAME
 from gallery.apps import GalleryConfig
-from gallery.suburls.home_gallery import HOME_GALLERY_ROOT_ROUTE
+from gallery.suburls.home_gallery import (HOME_GALLERY_ROOT_ROUTE,
+                                          URL_FILE_NAME_WITHOUT_EXTENSION as HOME_GALLERY_URL_FILE_NAME_WITHOUT_EXTENSION)
 from django.urls.conf import path, include
 
 app_name = GalleryConfig.name
@@ -9,4 +10,10 @@ _BASE_INCLUDE_PATH = '{}.{}.'.format(
     app_name,
     URL_FILE_NAME)
 
-urlpatterns = []
+urlpatterns = [
+    # Routes for Home Gallery
+    path(
+        HOME_GALLERY_ROOT_ROUTE,
+        include(
+            _BASE_INCLUDE_PATH + HOME_GALLERY_URL_FILE_NAME_WITHOUT_EXTENSION))
+]
