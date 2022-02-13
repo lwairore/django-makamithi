a505c17e21d59592ed88ccebcd61dd04029a40b0
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 17:43:15 +0300

    - Initialize constant '_LIST_GALLERY_URL_ROUTE'.

--- gallery/suburls/home_gallery.py
+++ gallery/suburls/home_gallery.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 HOME_GALLERY_ROOT_ROUTE = 'list/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_LIST_GALLERY_URL_ROUTE = ''
