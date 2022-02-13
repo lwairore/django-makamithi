e3e816970fa659efaf28642a76540b99c962ef6e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 14:39:32 +0300

    - Create private attribute '_serializer_class'.

--- home_two/views/gallery_section.py
+++ home_two/views/gallery_section.py
@@ -1,3 +1,4 @@
+from home_two.serializers.gallery_section import RetrieveGallerySectionModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,3 +6,4 @@ from rest_framework.views import APIView
 
 class RetrieveGallerySectionAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveGallerySectionModelSerializer
