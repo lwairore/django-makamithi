f02d6facd7bf8ffc35485c335d4f37a5dea88bf8
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 09:54:32 +0300

    - Overwrite field 'image'.

--- home_two/serializers/seo.py
+++ home_two/serializers/seo.py
@@ -1,9 +1,11 @@
+from social_sharing.serializers.image_aux_data import ImageAuxDataModelSerializer
 from home_two.submodels.seo import HomeSEODetailsModel
 from rest_framework.serializers import ModelSerializer
 
 
-
 class RetrieveHomeSEODetailsSerializer(ModelSerializer):
+    image = ImageAuxDataModelSerializer(required=False)
+
     class Meta:
         model = HomeSEODetailsModel
         fields = ('title', 'keywords', 'description', 'image',
