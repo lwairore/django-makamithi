01b1c90871ec468be50a469047fcebd8cc2ca154
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 18:25:52 +0300

    - Make field 'image'.

--- gallery/subadmins/gallery.py
+++ gallery/subadmins/gallery.py
@@ -1,9 +1,21 @@
 from gallery.submodels import GalleryModel
 from django.contrib.admin import ModelAdmin, register
+from django.forms import ModelForm
+
+
+class GalleryModelForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(GalleryModelForm, self).__init__(*args, **kwargs)
+        self.fields['image'].required = True
+
+    class Meta:
+        model = GalleryModel
+        fields = '__all__'
 
 
 @register(GalleryModel)
 class GalleryModelAdmin(ModelAdmin):
+    form = GalleryModelForm
     list_display = ('id', 'image',)
     date_hierarchy = 'created_at'
     raw_id_fields = ('image',)
