40c4233ef5178443440b9c6e4622a4a916911f9b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 13:24:10 +0300

    - Create model CoreValueModel

--- home_two/submodels/__init__.py
+++ home_two/submodels/__init__.py
@@ -5,3 +5,4 @@ from .our_feature_section import FeatureSectionModel
 from .visit_now_cta_section import VisitNowCtaSectionModel
 from .why_choose_us_section import WhyChooseUsSectionModel
 from .seo import HomeSEODetailsModel
+from .core_value import CoreValueModel
---
+++ home_two/submodels/core_value.py
@@ -0,0 +1,20 @@
+from django.db.models import Model
+from django.db.models.deletion import PROTECT
+from django.db.models.fields import CharField, DateTimeField, TextField
+from django.db.models.fields.related import ForeignKey
+
+class CoreValueModel(Model):
+    title = CharField(max_length=90)
+    description = TextField(max_length=255, blank=True, null=True)
+    image = ForeignKey(
+        'PhotoModel', blank=True, null=True, on_delete=PROTECT)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = 'Core value'
+        verbose_name_plural = 'Core values'
+        
