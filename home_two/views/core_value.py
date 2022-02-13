7f61680d58a4524f0cb008aea2b8d269219af9bc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 13:35:22 +0300

    - Define method '_list_core_value_kueryset'.

--- home_two/views/core_value.py
+++ home_two/views/core_value.py
@@ -1,3 +1,5 @@
+from django.db.models.query import QuerySet
+from home_two.submodels.core_value import CoreValueModel
 from home_two.serializers.core_value import CoreValueModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
@@ -6,3 +8,10 @@ from rest_framework.views import APIView
 class CoreValueAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = CoreValueModelSerializer
+
+    def _list_core_value_kueryset(self):
+        core_value_kueryset: QuerySet = CoreValueModel.objects\
+            .order_by().only('title', 'description', 'image__image',
+                             'image__caption', 'id',)
+
+        return core_value_kueryset
