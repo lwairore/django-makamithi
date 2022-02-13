0b96bfd711e4e755d920c17ba5428da076241439
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 16:10:17 +0300

    - Create model serializer '_RetrievePhotoModelSerializer'.

--- gallery/serializers/home_gallery.py
+++ gallery/serializers/home_gallery.py
@@ -1,5 +1,11 @@
+from home_two.submodels.preview_item import PhotoModel
 from gallery.submodels.gallery import GalleryModel
 from rest_framework.serializers import ModelSerializer
+
+class _RetrievePhotoModelSerializer(ModelSerializer):
+    class Meta:
+        model = PhotoModel
+        fields = ('image', 'caption',)
 
 class ListHomeGallerySerializer(ModelSerializer):
     class Meta:
