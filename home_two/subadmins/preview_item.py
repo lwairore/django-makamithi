0fee59d85a57b1169114e8a70dff43c5f2c87ed3
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 09:43:13 +0300

    - Update model admin 'PhotoModelAdmin'.

--- home_two/subadmins/preview_item.py
+++ home_two/subadmins/preview_item.py
@@ -4,6 +4,7 @@ from django.contrib.admin import ModelAdmin, register
 
 @register(PhotoModel)
 class PhotoModelAdmin(ModelAdmin):
-    list_display = ('id', 'image',)
+    list_display = ('id', 'image', 'width', 'height',)
     date_hierarchy = 'created_at'
-    list_filter = ('modified_date',)
+    list_filter = ('modified_date', 'created_at')
+    search_fields = ('caption', 'width', 'height',)
