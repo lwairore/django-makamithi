610733c7d301031b8b8c12670b4eea71ce9d4204
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 10:41:33 +0300

    - Reassign the value for constant 'HOME_SEO_DETAILS_ROOT_ROUTE'.

--- home_two/suburls/seo.py
+++ home_two/suburls/seo.py
@@ -3,7 +3,7 @@ from django.urls.conf import path
 from custom_utils.get_file_name_util import get_file_name
 
 
-HOME_SEO_DETAILS_ROOT_ROUTE = 'retrieve/seo/home/'
+HOME_SEO_DETAILS_ROOT_ROUTE = 'retrieve/seo/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
