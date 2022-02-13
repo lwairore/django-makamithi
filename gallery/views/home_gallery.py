fcf87f38c7afee165fda023d745256d8ac11ddf9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 16:19:07 +0300

    - Create api view 'ListHomeGalleryAPIView'.

--- gallery/views/home_gallery.py
+++ gallery/views/home_gallery.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class ListHomeGalleryAPIView(APIView):
+    permission_classes = (AllowAny,)
