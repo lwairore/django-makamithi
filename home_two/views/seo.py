aa6b9f7ad098befe2b1a60d82f49fbbc2e8a151a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-13 09:55:42 +0300

    - Define method '_get_home_seo_details_instance'.

--- home_two/views/seo.py
+++ home_two/views/seo.py
@@ -1,3 +1,4 @@
+from home_two.submodels.seo import HomeSEODetailsModel
 from home_two.serializers.seo import RetrieveHomeSEODetailsSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -7,3 +8,13 @@ from rest_framework.views import APIView
 class RetrieveHomeSEODetailsAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveHomeSEODetailsSerializer
+
+    def _get_home_seo_details_instance(self):
+        home_seo_details_instance = HomeSEODetailsModel.objects\
+            .select_related('image')\
+            .order_by()\
+            .only('title', 'keywords', 'description',
+                  'image__width', 'image__height', 'image__image', 'image__caption',
+                  'url', 'type', 'author', 'section', 'published', 'modified')
+
+        return home_seo_details_instance
