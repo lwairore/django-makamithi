6ac91fedea820b32552f9df8243f43c76cd94674
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 07:55:37 +0300

    - Include routes for 'site breadcrumb' section.

--- site_breadcrumb/urls.py
+++ site_breadcrumb/urls.py
@@ -1,6 +1,16 @@
+from django.conf.urls import include
+from site_breadcrumb.suburls.site_breadcrumb import (SITE_BREADCRUMB_ROOT_ROUTE,
+                                                     URL_FILE_NAME_WITHOUT_EXTENSION as SITE_BREADCRUMB_URL_FILE_NAME_WITHOUT_EXTENSION)
+from django.urls.conf import path
 from site_breadcrumb.apps import SiteBreadcrumbConfig
 
 
 app_name = SiteBreadcrumbConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    # Routes for Site-Breadcrumb
+    path(SITE_BREADCRUMB_ROOT_ROUTE,
+         include(
+             _BASE_INCLUDE_PATH + SITE_BREADCRUMB_URL_FILE_NAME_WITHOUT_EXTENSION
+         ))
+]
