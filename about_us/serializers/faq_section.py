65ea9c16ace62bcc97843bfc3a739b8196da3bcd
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 13:46:51 +0300

    - Create model serializer 'RetrieveFaqSectionModelSerializer'.

--- about_us/serializers/faq_section.py
+++ about_us/serializers/faq_section.py
@@ -1,4 +1,9 @@
+from about_us.submodels.faq_section import FaqSectionModel
 from home_two.models import PhotoModel
-from about_us.submodels.ap_about_section import ApAboutSectionModel
 from rest_framework.serializers import ModelSerializer
 
+
+class RetrieveFaqSectionModelSerializer(ModelSerializer):
+    class Meta:
+        model = FaqSectionModel
+        fields = ('title', 'background_image',)
