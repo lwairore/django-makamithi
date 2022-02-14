11df5185f8f4131458ecdfbfc8dc6ede57e8361f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 08:04:29 +0300

    - Define method 'get_parent_directory_name_of_current_directory'.

--- custom_utils/get_file_name_util.py
+++ custom_utils/get_file_name_util.py
@@ -1,4 +1,15 @@
 import os
+from os.path import basename
+
+from django.urls.conf import path
+
+
+def get_parent_directory_name_of_current_directory():
+    current_working_directory = os.getcwd()
+
+    parent_directory_name = basename(current_working_directory)
+
+    return parent_directory_name
 
 
 def get_file_name(path):
