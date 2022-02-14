30dd5bb3ebe9209260d3492365cfa62d93d75a06
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 11:58:08 +0300

    - Include 'URLConfs' for section 'SEO'.

--- about_us/urls.py
+++ about_us/urls.py
@@ -1,3 +1,6 @@
+from about_us.suburls.seo import (
+    ABOUT_US_SEO_DETAILS_ROOT_ROUTE,
+    URL_FILE_NAME_WITHOUT_EXTENSION as ABOUT_US_SEO_URL_FILE_NAME_WITHOUT_EXTENSION)
 from about_us.suburls.ap_about_section import (AP_ABOUT_SECTION_ROOT_ROUTE,
                                                URL_FILE_NAME_WITHOUT_EXTENSION as AP_ABOUT_SECTION_URL_FILE_NAME_WITHOUT_EXTENSION)
 from django.urls.conf import include, path
@@ -15,5 +18,10 @@ urlpatterns = [
     # Routes for section Ap About
     path(AP_ABOUT_SECTION_ROOT_ROUTE,
          include(
-             _BASE_INCLUDE_PATH+AP_ABOUT_SECTION_URL_FILE_NAME_WITHOUT_EXTENSION))
+             _BASE_INCLUDE_PATH+AP_ABOUT_SECTION_URL_FILE_NAME_WITHOUT_EXTENSION)),
+
+    # Routes for section About SEO
+    path(ABOUT_US_SEO_DETAILS_ROOT_ROUTE,
+         include(
+             _BASE_INCLUDE_PATH+ABOUT_US_SEO_URL_FILE_NAME_WITHOUT_EXTENSION))
 ]
