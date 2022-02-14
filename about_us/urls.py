28c0e562c8008c814bfc596627def7d8fb7dea5d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:24:10 +0300

    - Include 'URLConfs' for section 'ap about'.

--- about_us/urls.py
+++ about_us/urls.py
@@ -1,3 +1,6 @@
+from about_us.suburls.ap_about_section import (AP_ABOUT_SECTION_ROOT_ROUTE,
+                                               URL_FILE_NAME_WITHOUT_EXTENSION as AP_ABOUT_SECTION_URL_FILE_NAME_WITHOUT_EXTENSION)
+from django.urls.conf import include, path
 from about_us.suburls import URL_FILE_NAME
 from about_us.apps import AboutUsConfig
 
@@ -7,5 +10,10 @@ app_name = AboutUsConfig.name
 _BASE_INCLUDE_PATH = '{}.{}.'.format(
     app_name,
     URL_FILE_NAME)
-    
-urlpatterns = []
+
+urlpatterns = [
+    # Routes for section Ap About
+    path(AP_ABOUT_SECTION_ROOT_ROUTE,
+         include(
+             _BASE_INCLUDE_PATH+AP_ABOUT_SECTION_URL_FILE_NAME_WITHOUT_EXTENSION))
+]
