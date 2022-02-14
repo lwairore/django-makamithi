f6b4728b5c7b2aac6832b1c8297290981c4bffba
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 13:47:43 +0300

    - Create serializer '_RetrievePhotoModelSerializer'.

--- about_us/serializers/faq_section.py
+++ about_us/serializers/faq_section.py
@@ -3,6 +3,12 @@ from home_two.models import PhotoModel
 from rest_framework.serializers import ModelSerializer
 
 
+class _RetrievePhotoModelSerializer(ModelSerializer):
+    class Meta:
+        model = PhotoModel
+        fields = ('image', 'caption',)
+
+
 class RetrieveFaqSectionModelSerializer(ModelSerializer):
     class Meta:
         model = FaqSectionModel
