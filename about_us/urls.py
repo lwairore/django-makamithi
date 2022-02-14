bdd0d53cb364a0568daaf3e3e27411286eee48df
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:21:38 +0300

    - Initialize private constant '_BASE_INCLUDE_PATH'.

--- about_us/urls.py
+++ about_us/urls.py
@@ -1,6 +1,11 @@
+from about_us.suburls import URL_FILE_NAME
 from about_us.apps import AboutUsConfig
 
 
 app_name = AboutUsConfig.name
 
+_BASE_INCLUDE_PATH = '{}.{}.'.format(
+    app_name,
+    URL_FILE_NAME)
+    
 urlpatterns = []
