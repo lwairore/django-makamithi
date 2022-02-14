b5e1739366fdaed7d1480449b91842616ba9037c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 13:51:19 +0300

    - Define method '_get_faq_section_instance'.

--- about_us/views/faq_section.py
+++ about_us/views/faq_section.py
@@ -1,3 +1,4 @@
+from about_us.submodels.faq_section import FaqSectionModel
 from about_us.serializers.faq_section import RetrieveFaqSectionModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -7,3 +8,12 @@ from rest_framework.views import APIView
 class RetrieveFaqSectionAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveFaqSectionModelSerializer
+
+    def _get_faq_section_instance(self):
+        faq_section_instance = FaqSectionModel.objects\
+            .only('title', 'background_image__image', 'background_image__caption',)\
+            .select_related('background_image')\
+            .order_by()\
+            .first()
+
+        return faq_section_instance
