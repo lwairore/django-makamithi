a0986a5099c5746bc80b9e3f99eb05dee86c752e
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 09:48:44 +0300

    - Create model serializer '_RetrievePhotoModelSerializer'.

--- about_us/serializers/ap_about_section.py
+++ about_us/serializers/ap_about_section.py
@@ -1,6 +1,11 @@
+from home_two.submodels.preview_item import PhotoModel
 from about_us.submodels.ap_about_section import ApAboutSectionModel
 from rest_framework.serializers import ModelSerializer
 
+class _RetrievePhotoModelSerializer(ModelSerializer):
+    class Meta:
+        model = PhotoModel
+        fields = ('image', 'caption',)
 
 class ApAboutSectionModelSerializer(ModelSerializer):
     class Meta:
