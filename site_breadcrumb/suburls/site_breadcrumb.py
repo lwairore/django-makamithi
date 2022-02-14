9d39a073f66a9f27bf5cba0d644cbedc4c9c0a35
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 07:52:22 +0300

    - Initialize constant '_RETRIEVE_SITE_BREADCRUMB_URL_ROUTE'.

--- site_breadcrumb/suburls/site_breadcrumb.py
+++ site_breadcrumb/suburls/site_breadcrumb.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 SITE_BREADCRUMB_ROOT_ROUTE = 'retrieve/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_RETRIEVE_SITE_BREADCRUMB_URL_ROUTE = ''
