1a4597097309d3fa54f2a544c01fb95657bd5043
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 14:03:45 +0300

    - Map api view 'RetrieveFaqSectionAPIView' to a 'URLConf'.

--- about_us/suburls/faq_section.py
+++ about_us/suburls/faq_section.py
@@ -1,5 +1,6 @@
+from about_us.views.faq_section import RetrieveFaqSectionAPIView
 from custom_utils.get_file_name_util import get_file_name
-
+from django.urls import path
 
 FAQ_SECTION_ROOT_ROUTE = 'retrieve/section/faq/'
 
@@ -7,4 +8,7 @@ URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _RETRIEVE_FAQ_SECTION_URL_ROUTE = ''
 
-urlpatterns = []
+urlpatterns = [
+    path(_RETRIEVE_FAQ_SECTION_URL_ROUTE,
+         RetrieveFaqSectionAPIView.as_view()),
+]
