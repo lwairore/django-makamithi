2a2186d558eb95d30e40080f2860e34f8d256263
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:24:50 +0300

    - Initialize private constant '_RETRIEVE_API_ABOUT_SECTION_URL_ROUTE'.

--- about_us/suburls/ap_about_section.py
+++ about_us/suburls/ap_about_section.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 AP_ABOUT_SECTION_ROOT_ROUTE = 'retrieve/section/ap-about/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_RETRIEVE_API_ABOUT_SECTION_URL_ROUTE = ''
