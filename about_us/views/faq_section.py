4037cc2bc2390adb9b6df3977c0533b94cc7a07d
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 13:49:32 +0300

    - Add and configure private attribute '_serializer_class'.

--- about_us/views/faq_section.py
+++ about_us/views/faq_section.py
@@ -1,6 +1,9 @@
+from about_us.serializers.faq_section import RetrieveFaqSectionModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
 class RetrieveFaqSectionAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveFaqSectionModelSerializer
