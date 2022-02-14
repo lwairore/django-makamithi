4fe335a01f4edc9c068d4f72881e03fc32c00050
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 07:53:12 +0300

    - Map api view 'RetrieveSiteBreadcrumbModelAPIView' to a 'URLConf'.

--- site_breadcrumb/suburls/site_breadcrumb.py
+++ site_breadcrumb/suburls/site_breadcrumb.py
@@ -1,3 +1,5 @@
+from site_breadcrumb.views.site_breadcrumb import RetrieveSiteBreadcrumbModelAPIView
+from django.urls.conf import path
 from custom_utils.get_file_name_util import get_file_name
 
 
@@ -7,4 +9,7 @@ URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _RETRIEVE_SITE_BREADCRUMB_URL_ROUTE = ''
 
-urlpatterns = []
+urlpatterns = [
+    path(_RETRIEVE_SITE_BREADCRUMB_URL_ROUTE,
+         RetrieveSiteBreadcrumbModelAPIView.as_view()),
+]
