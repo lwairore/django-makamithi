6ba15721a84c55a62bc66643c0d45abd7f8d1d41
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 09:53:54 +0300

    - Define method '_get_ap_about_section_instance'.

--- about_us/views / ap_about_section.py
+++ about_us/views / ap_about_section.py
@@ -1,3 +1,4 @@
+from about_us.submodels.ap_about_section import ApAboutSectionModel
 from about_us.serializers.ap_about_section import RetrieveApAboutSectionModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -7,3 +8,13 @@ from rest_framework.views import APIView
 class RetrieveApAboutSectionAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveApAboutSectionModelSerializer
+
+    def _get_ap_about_section_instance(self):
+        ap_about_section_instance = ApAboutSectionModel.objects\
+            .only('section_image__image', 'section_image__caption',
+                  'heading', 'subheading', 'description',)\
+            .select_related('section_image')\
+            .order_by()\
+            .first()
+
+        return ap_about_section_instance
