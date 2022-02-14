309b035136671a335b2e06773cf682a57f40579f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 11:47:48 +0300

    - Add and configure private attribute '_serializer_class'.

--- about_us/views/seo.py
+++ about_us/views/seo.py
@@ -1,3 +1,4 @@
+from about_us.serializers.seo import RetrieveAboutUsSEODetailsModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,3 +6,4 @@ from rest_framework.views import APIView
 
 class RetrieveAboutUsSEODetailsAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveAboutUsSEODetailsModelSerializer
