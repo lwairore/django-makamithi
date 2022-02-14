6ffa46345f8a79aa27ca4d5e9c046b0c59eb0892
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 08:23:32 +0300

    - Enable delete option.

--- home_two/subadmins/banner_ad.py
+++ home_two/subadmins/banner_ad.py
@@ -6,6 +6,3 @@ from django.contrib.admin import ModelAdmin, register
 class BannerAdModelAdmin(ModelAdmin):
     list_display = ('title', 'photo',)
     raw_id_fields = ('photo',)
-
-    def has_delete_permission(self, request, obj=None):
-        return False
