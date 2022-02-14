4576079622b9b67669344bca58506408278042e2
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 11:48:23 +0300

    - Define private method '_get_about_us_seo_details_instance'.

--- about_us/views/seo.py
+++ about_us/views/seo.py
@@ -1,3 +1,4 @@
+from about_us.submodels.seo import AboutUsSEODetailsModel
 from about_us.serializers.seo import RetrieveAboutUsSEODetailsModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -7,3 +8,14 @@ from rest_framework.views import APIView
 class RetrieveAboutUsSEODetailsAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveAboutUsSEODetailsModelSerializer
+
+    def _get_about_us_seo_details_instance(self):
+        about_us_seo_details_instance = AboutUsSEODetailsModel.objects\
+            .select_related('image')\
+            .order_by()\
+            .only('title', 'keywords', 'description',
+                  'image__width', 'image__height', 'image__image', 'image__caption',
+                  'type', 'author', 'section', 'published', 'modified')\
+            .first()
+
+        return about_us_seo_details_instance
