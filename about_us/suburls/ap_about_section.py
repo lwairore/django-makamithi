0e05b4cef8852ae771d90dd467e53ef63b09f259
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 10:22:59 +0300

    - Update constant 'AP_ABOUT_SECTION_ROOT_ROUTE'.

--- about_us/suburls/ap_about_section.py
+++ about_us/suburls/ap_about_section.py
@@ -1,6 +1,6 @@
 from custom_utils.get_file_name_util import get_file_name
 
 
-AP_ABOUT_SECTION_ROOT_ROUTE = 'retrieve/'
+AP_ABOUT_SECTION_ROOT_ROUTE = 'retrieve/section/ap-about/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
