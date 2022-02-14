e32efe1be41feeb3a7e3a778f7609a986fec346c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 08:21:45 +0300

    - Remove add option if one instance has been added.

--- site_breadcrumb/subadmins/site_breadcrumb.py
+++ site_breadcrumb/subadmins/site_breadcrumb.py
@@ -21,5 +21,14 @@ class SiteBreadcrumbModelAdmin(ModelAdmin):
     raw_id_fields = ('background_image',)
     list_filter = ('modified_date', 'created_at',)
 
-    def has_delete_permission(self, request, obj = None):
+    def has_delete_permission(self, request, obj=None):
         return False
+
+    def has_add_permission(self, request):
+        # check if generally has add permission
+        should_add_instance = super().has_add_permission(request)
+        # set add permission to False, if object already exists
+        if should_add_instance and SiteBreadcrumbModel.objects.only('pk').exists():
+            should_add_instance = False
+
+        return should_add_instance
