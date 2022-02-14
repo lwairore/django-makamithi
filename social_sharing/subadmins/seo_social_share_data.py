3ccc21a87d5ab2983e3d4498582b3da8b00cbace
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 12:05:37 +0300

    - Remove unnecessary option 'search_fields'.

--- social_sharing/subadmins/seo_social_share_data.py
+++ social_sharing/subadmins/seo_social_share_data.py
@@ -4,8 +4,6 @@ from django.contrib.admin import ModelAdmin
 class SeoSocialShareDataModelAdmin(ModelAdmin):
     list_display = ('title', 'keywords', 'type',
                     'author', 'section', 'published', 'modified',)
-    search_fields = ('title', 'keywords', 'description',
-                     'type', 'author', 'section')
     raw_id_fields = ('image',)
     date_hierarchy = 'published'
     list_filter = ('modified', 'published',)
