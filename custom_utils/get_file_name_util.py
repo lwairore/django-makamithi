89a858b397ea8dd2704c3ba4029c0e0e4ba49a7c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 09:57:50 +0300

    - Update function "get_parent_directory_name_of_current_directory".

--- custom_utils/get_file_name_util.py
+++ custom_utils/get_file_name_util.py
@@ -2,9 +2,7 @@ import os
 from os.path import basename
 
 
-def get_parent_directory_name_of_current_directory():
-    current_working_directory = os.getcwd()
-
+def get_parent_directory_name_of_current_directory(current_working_directory: str):
     parent_directory_name = basename(current_working_directory)
 
     return parent_directory_name
