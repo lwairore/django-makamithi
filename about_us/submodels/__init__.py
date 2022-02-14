f4a204096d0aaf8329cc6c57fd4866ae97c63642
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 13:28:58 +0300

    - Create model FaqSectionModel

--- about_us/submodels/__init__.py
+++ about_us/submodels/__init__.py
@@ -1,2 +1,3 @@
 from .ap_about_section import ApAboutSectionModel
 from .seo import AboutUsSEODetailsModel
+from .faq_section import FaqSectionModel
---
+++ about_us/submodels/faq_section.py
@@ -0,0 +1,33 @@
+from django.core.exceptions import ValidationError
+from django.db.models.fields import CharField, DateTimeField, TextField
+from home_two.submodels import PhotoModel
+from django.db.models import Model
+from django.db.models.deletion import PROTECT
+from django.db.models.fields.related import ForeignKey
+
+
+class FaqSectionModel(Model):
+    title = CharField(max_length=70)
+    background_image = ForeignKey(
+        PhotoModel, blank=True, null=True, on_delete=PROTECT)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def clean(self):
+        if FaqSectionModel.objects.exists() and not self.pk:
+            raise ValidationError("You can only have one company")
+
+    def save(self, *args, **kwargs):
+        if not self.pk and FaqSectionModel.objects.exists():
+            # if you'll not check for self.pk
+            # then error will also raised in update of exists model
+            raise ValidationError(
+                'There can be only one "FaqSectionModel" instance')
+        return super(FaqSectionModel, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Faq section'
+        verbose_name_plural = 'Faq sections'
