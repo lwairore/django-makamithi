137cb3d41161b7595d5a2588ebf0acadd53f3a90
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 09:49:15 +0300

    - Overwrite field 'section_image'.

--- about_us/serializers/ap_about_section.py
+++ about_us/serializers/ap_about_section.py
@@ -2,12 +2,16 @@ from home_two.submodels.preview_item import PhotoModel
 from about_us.submodels.ap_about_section import ApAboutSectionModel
 from rest_framework.serializers import ModelSerializer
 
+
 class _RetrievePhotoModelSerializer(ModelSerializer):
     class Meta:
         model = PhotoModel
         fields = ('image', 'caption',)
 
+
 class ApAboutSectionModelSerializer(ModelSerializer):
+    section_image = _RetrievePhotoModelSerializer(required=False)
+
     class Meta:
         model = ApAboutSectionModel
         fields = ('heading', 'subheading', 'description',
