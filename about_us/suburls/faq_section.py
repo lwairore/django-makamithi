b89d43e8193ccf1e362a6d65df3559b64607dec9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 14:02:29 +0300

    - Initialize constant '_RETRIEVE_FAQ_SECTION_URL_ROUTE'.

--- about_us/suburls/faq_section.py
+++ about_us/suburls/faq_section.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 FAQ_SECTION_ROOT_ROUTE = 'retrieve/section/faq/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_RETRIEVE_FAQ_SECTION_URL_ROUTE = ''
