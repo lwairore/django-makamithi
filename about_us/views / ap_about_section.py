8628c0eb47da672ed9c76532f9f5820bc916fb0c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 09:52:02 +0300

    - Create and configure private attribute '_serializer_class'.

--- about_us/views / ap_about_section.py
+++ about_us/views / ap_about_section.py
@@ -1,3 +1,4 @@
+from about_us.serializers.ap_about_section import RetrieveApAboutSectionModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,3 +6,4 @@ from rest_framework.views import APIView
 
 class RetrieveApAboutSectionAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveApAboutSectionModelSerializer
