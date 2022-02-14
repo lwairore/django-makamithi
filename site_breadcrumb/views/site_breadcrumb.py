6f366535a39c942caf7b8f6fe7efac99885a0518
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 07:48:10 +0300

    - Define method '_get_site_breadcrumb_instance'.

--- site_breadcrumb/views/site_breadcrumb.py
+++ site_breadcrumb/views/site_breadcrumb.py
@@ -1,3 +1,4 @@
+from site_breadcrumb.submodels.site_breadcrumb import SiteBreadcrumbModel
 from site_breadcrumb.serializers.site_breadcrumb import RetrieveSiteBreadcrumbModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -7,4 +8,12 @@ from rest_framework.views import APIView
 class RetrieveSiteBreadcrumbModelAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveSiteBreadcrumbModelSerializer
-    
+
+    def _get_site_breadcrumb_instance(self):
+        site_breadcrumb_instance = SiteBreadcrumbModel.objects\
+            .only(
+                'background_image__image', 'background_image__caption',)\
+            .select_related('background_image')\
+            .order_by().first()
+
+        return site_breadcrumb_instance
