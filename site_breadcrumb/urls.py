613fc18d4d995ad1d2886b554bc590954c243b85
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-14 08:06:21 +0300

    - Initialize constant '_BASE_INCLUDE_PATH'.

--- site_breadcrumb/urls.py
+++ site_breadcrumb/urls.py
@@ -1,3 +1,4 @@
+from site_breadcrumb.suburls import URL_FILE_NAME
 from django.conf.urls import include
 from site_breadcrumb.suburls.site_breadcrumb import (SITE_BREADCRUMB_ROOT_ROUTE,
                                                      URL_FILE_NAME_WITHOUT_EXTENSION as SITE_BREADCRUMB_URL_FILE_NAME_WITHOUT_EXTENSION)
@@ -6,6 +7,10 @@ from site_breadcrumb.apps import SiteBreadcrumbConfig
 
 
 app_name = SiteBreadcrumbConfig.name
+
+_BASE_INCLUDE_PATH = '{}.{}.'.format(
+    app_name,
+    URL_FILE_NAME)
 
 urlpatterns = [
     # Routes for Site-Breadcrumb
