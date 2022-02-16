af2038431de3d4748b4bf0cbf1c77d89d13c36cb
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 12:05:03 +0300

    - Rename model serializer 'FaqModelSerializer' to 'ListFaqModelSerializer'.

--- about_us/serializers/faq.py
+++ about_us/serializers/faq.py
@@ -2,7 +2,7 @@ from about_us.models import FaqModel
 from rest_framework.serializers import ModelSerializer
 
 
-class FaqModelSerializer(ModelSerializer):
+class RetrieveFaqModelSerializer(ModelSerializer):
     class Meta:
         model = FaqModel
         fields = ('question', 'answer',)
