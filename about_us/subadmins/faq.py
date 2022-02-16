31e167219c337e3f7c0aa3d2c181ad0151ef8c42
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 12:02:04 +0300

    - Create model admin 'FaqModelAdmin'.

--- about_us/subadmins/faq.py
+++ about_us/subadmins/faq.py
@@ -7,3 +7,5 @@ class FaqModelAdmin(ModelAdmin):
     list_display = ('question', 'modified_date',
                     'created_at', )
     date_hierarchy = 'created_at'
+    list_filter = ('modified_date', 'created_at',)
+    
