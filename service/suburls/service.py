a4472a234b0e20cd53fedd197336a1241a3ca509
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:36:10 +0300

    - Rename private constant '_LIST_SERVICE_ROUTE' to '_LIST_HOME_SERVICE_ROUTE'.

--- service/suburls/service.py
+++ service/suburls/service.py
@@ -5,10 +5,10 @@ from custom_utils.get_file_name_util import get_file_name
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
-_LIST_SERVICE_ROUTE = ''
+_LIST_HOME_SERVICE_ROUTE = ''
 
 LIST_SERVICE_ROOT_ROUTE = 'list/'
 
 urlpatterns = [
-    path(_LIST_SERVICE_ROUTE, ListHomeServiceAPIView.as_view()),
+    path(_LIST_HOME_SERVICE_ROUTE, ListHomeServiceAPIView.as_view()),
 ]
