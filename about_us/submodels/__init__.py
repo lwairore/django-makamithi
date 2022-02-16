c1b020c5b5a26e8607df106a00f9145c97509f8a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:09:06 +0300

    - Create model WhatWeDoSectionModel

--- about_us/submodels/__init__.py
+++ about_us/submodels/__init__.py
@@ -2,3 +2,4 @@ from .ap_about_section import ApAboutSectionModel
 from .seo import AboutUsSEODetailsModel
 from .faq_section import FaqSectionModel
 from .faq import FaqModel
+from .what_we_do_section import WhatWeDoSectionModel
