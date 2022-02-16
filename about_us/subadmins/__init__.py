288b6d4da7892ab5af48e2de8eb2748bfe7d799c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:12:31 +0300

    - Import model admin 'WhatWeDoSectionModelAdmin'.

--- about_us/subadmins/__init__.py
+++ about_us/subadmins/__init__.py
@@ -2,3 +2,4 @@ from .ap_about_section import ApAboutSectionModelAdmin
 from .seo import AboutUsSEODetailsModelAdmin
 from .faq_section import FaqSectionModelAdmin
 from .faq import FaqModelAdmin
+from .what_we_do_section import WhatWeDoSectionModelAdmin
