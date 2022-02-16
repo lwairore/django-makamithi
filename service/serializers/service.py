3494f48bd9035a5e62ca2cf7ba1b1a7b4cf891ac
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:34:07 +0300

    - Create model serializer 'RetrieveAboutServiceModelSerializer'.

--- service/serializers/service.py
+++ service/serializers/service.py
@@ -15,3 +15,11 @@ class RetrieveHomeServiceModelSerializer(ModelSerializer):
     class Meta:
         model = ServiceModel
         fields = ('home_photo', 'id', 'title', 'summary')
+
+
+class RetrieveAboutServiceModelSerializer(ModelSerializer):
+    about_photo = _RetrievePhotoModelModelSerializer(required=False)
+
+    class Meta:
+        model = ServiceModel
+        fields = ('about_photo', 'id', 'title', 'summary')
