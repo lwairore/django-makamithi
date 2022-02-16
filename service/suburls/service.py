807fd08d446389f7209a7b566b22daee7d907e20
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:37:54 +0300

    - Initialize constant '_LIST_ABOUT_SERVICE_ROUTE'.

--- service/suburls/service.py
+++ service/suburls/service.py
@@ -7,6 +7,8 @@ URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _LIST_HOME_SERVICE_ROUTE = 'home/'
 
+_LIST_ABOUT_SERVICE_ROUTE = 'about/'
+
 LIST_SERVICE_ROOT_ROUTE = 'list/'
 
 urlpatterns = [
