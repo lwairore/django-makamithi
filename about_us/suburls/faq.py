f15ffc37cb10736aab9ba72d9daa15a9a577d81c
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 15:09:30 +0300

    - Map api view 'ListFaqModelAPIView'.

--- about_us/suburls/faq.py
+++ about_us/suburls/faq.py
@@ -1,3 +1,5 @@
+from about_us.views.faq import ListFaqModelAPIView
+from django.urls import path
 from custom_utils.get_file_name_util import get_file_name
 
 
@@ -6,3 +8,8 @@ FAQ_ROOT_ROUTE = 'list/faq/'
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _LIST_FAQ_URL_ROUTE = ''
+
+urlpatterns = [
+    path(_LIST_FAQ_URL_ROUTE,
+         ListFaqModelAPIView.as_view()),
+]
