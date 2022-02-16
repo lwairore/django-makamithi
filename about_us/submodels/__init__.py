63d3f151e75bc998e17d058fbd0b491aea9876e1
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 11:59:09 +0300

    - Create model FaqModel

--- about_us/submodels/__init__.py
+++ about_us/submodels/__init__.py
@@ -1,3 +1,4 @@
 from .ap_about_section import ApAboutSectionModel
 from .seo import AboutUsSEODetailsModel
 from .faq_section import FaqSectionModel
+from .faq import FaqModel
---
+++ about_us/submodels/faq.py
@@ -0,0 +1,20 @@
+from django.core.exceptions import ValidationError
+from django.db.models import Model
+from django.db.models.deletion import PROTECT
+from django.db.models.fields import CharField, DateTimeField, TextField
+from django.db.models.fields.related import ForeignKey
+
+
+class FaqModel(Model):
+    question = CharField(max_length=150)
+    answer = TextField(max_length=500)
+
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.question
+
+    class Meta:
+        verbose_name = 'FAQ'
+        verbose_name = 'FAQs'
