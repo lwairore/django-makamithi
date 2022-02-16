479ed8181fa2fdab7f8d818830f8cbf18683f9ae
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:21:04 +0300

    - Map api view 'RetrieveWhatWeDoSectionAPIView' to a 'URLConf'.

--- about_us/suburls/what_we_do_section.py
+++ about_us/suburls/what_we_do_section.py
@@ -1,8 +1,14 @@
+from about_us.views.what_we_do_section import RetrieveWhatWeDoSectionAPIView
 from custom_utils.get_file_name_util import get_file_name
-
+from django.urls import path
 
 WHAT_WE_DO_SECTION_ROOT_ROUTE = 'retrieve/section/what-we-do/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _RETRIEVE_WHAT_WE_DO_SECTION_URL_ROUTE = ''
+
+urlpatterns = [
+    path(_RETRIEVE_WHAT_WE_DO_SECTION_URL_ROUTE,
+         RetrieveWhatWeDoSectionAPIView.as_view()),
+]
