25cb47cac626f443090694c33333c32bbcb986fc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:32:13 +0300

    - Update model admin 'ServiceModelAdmin'.

--- service/subadmins/service.py
+++ service/subadmins/service.py
@@ -4,8 +4,8 @@ from django.contrib.admin import ModelAdmin, register
 
 @register(ServiceModel)
 class ServiceModelAdmin(ModelAdmin):
-    list_display = ('title', 'home_photo',)
+    list_display = ('title', 'home_photo', 'about_photo',)
     search_fields = ('title', 'summary',)
-    raw_id_fields = ('home_photo',)
+    raw_id_fields = ('home_photo', 'about_photo',)
     date_hierarchy = 'modified_date'
     list_filter = ('created_at', 'modified_date',)
