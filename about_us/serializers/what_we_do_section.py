c4e2963e7cd74f2d011bb77cdba55887c3f6d2f4
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:15:29 +0300

    - Create model serializer '_RetrievePhotoModelSerializer'.

--- about_us/serializers/what_we_do_section.py
+++ about_us/serializers/what_we_do_section.py
@@ -1,5 +1,11 @@
+from home_two.submodels.preview_item import PhotoModel
 from about_us.submodels.what_we_do_section import WhatWeDoSectionModel
 from rest_framework.serializers import ModelSerializer
+
+class _RetrievePhotoModelSerializer(ModelSerializer):
+    class Meta:
+        model = PhotoModel
+        fields = ('image', 'caption',)
 
 
 class RetrieveWhatWeDoSectionModelSerializer(ModelSerializer):
