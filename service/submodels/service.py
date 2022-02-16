8a5b622ff8bb5e51e07f582e4926c89534d0da1a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:30:55 +0300

    - Add field about_photo to servicemodel     - Alter field home_photo on servicemodel

--- service/submodels/service.py
+++ service/submodels/service.py
@@ -7,7 +7,9 @@ from django.db.models.fields.related import ForeignKey
 
 class ServiceModel(Model):
     home_photo = ForeignKey(PhotoModel, blank=True,
-                            null=True, on_delete=PROTECT)
+                            null=True, on_delete=PROTECT, related_name='service_model_home_photo')
+    about_photo = ForeignKey(PhotoModel, blank=True,
+                            null=True, on_delete=PROTECT, related_name='service_model_about_photo')
     title = CharField(max_length=80)
     summary = TextField(max_length=120, blank=True, null=True)
     created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
