4c7d6b808c15e93ef9d5de1a5d6f87b33e5b99b1
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:35:32 +0300

    - Create api view 'ListAboutServiceAPIView'.

--- service/views/service.py
+++ service/views/service.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from service.submodels.service import ServiceModel
-from service.serializers.service import RetrieveHomeServiceModelSerializer
+from service.serializers.service import RetrieveAboutServiceModelSerializer, RetrieveHomeServiceModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
@@ -21,6 +21,29 @@ class ListHomeServiceAPIView(APIView):
     def get(self, request):
         service_kueryset = self._list_service()
 
-        service_kueryset_serializer = self._serializer_class(service_kueryset, many=True)
+        service_kueryset_serializer = self._serializer_class(
+            service_kueryset, many=True)
+
+        return Response(service_kueryset_serializer.data)
+
+
+class ListAboutServiceAPIView(APIView):
+    permission_classes = (AllowAny,)
+    _serializer_class = RetrieveAboutServiceModelSerializer
+
+    def _list_service(self):
+        service_kueryset = ServiceModel.objects\
+            .select_related('about_photo')\
+            .order_by()\
+            .only('about_photo__image', 'about_photo__caption', 'id', 'title', 'summary',)\
+            .all()
+
+        return service_kueryset
+
+    def get(self, request):
+        service_kueryset = self._list_service()
+
+        service_kueryset_serializer = self._serializer_class(
+            service_kueryset, many=True)
 
         return Response(service_kueryset_serializer.data)
