62ba88435a55ad0ca3c58d49494c65d39446df12
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-16 16:20:22 +0300

    - Initialize constant '_RETRIEVE_WHAT_WE_DO_SECTION_URL_ROUTE'.

--- about_us/suburls/what_we_do_section.py
+++ about_us/suburls/what_we_do_section.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 WHAT_WE_DO_SECTION_ROOT_ROUTE = 'retrieve/section/what-we-do/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_RETRIEVE_WHAT_WE_DO_SECTION_URL_ROUTE = ''
