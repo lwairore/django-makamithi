210c410c0abc0b761d60c1cd7b3184a9e51e4afe
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 15:40:24 +0300

    - Include url confs for section 'contact info'.

--- contact_us/urls.py
+++ contact_us/urls.py
@@ -1,3 +1,6 @@
+from contact_us.suburls.contact_info import (CONTACT_INFO_ROOT_ROUTE,
+                                             URL_FILE_NAME_WITHOUT_EXTENSION as CONTACT_INFO_URL_FILE_NAME_WITHOUT_EXTENSION)
+from django.urls import path, include
 from contact_us.suburls import URL_FILE_NAME
 from contact_us.apps import ContactUsConfig
 
@@ -7,4 +10,9 @@ _BASE_INCLUDE_PATH = '{}.{}.'.format(
     app_name,
     URL_FILE_NAME)
 
-urlpatterns = []
+urlpatterns = [
+    # Routes for section contact info
+    path(CONTACT_INFO_ROOT_ROUTE,
+         include(
+             _BASE_INCLUDE_PATH+CONTACT_INFO_URL_FILE_NAME_WITHOUT_EXTENSION)),
+]
