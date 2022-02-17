914029d7d118063ed5138e4ca06729bb80a2d040
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 21:24:35 +0300

    - Create model admin 'PlanModelAdmin'.

--- service/subadmins/plan.py
+++ service/subadmins/plan.py
@@ -1,11 +1,19 @@
-from service.submodels.plan import TypeOfPlanModel
+from service.submodels.plan import PlanModel, TypeOfPlanModel
 from django.contrib.admin import ModelAdmin, register
 
 
 @register(TypeOfPlanModel)
 class TypeOfPlanModelAdmin(ModelAdmin):
-    list_display = ('title', 'description',)
+    list_display = ('title',)
     search_fields = ('title', 'description',)
-    raw_id_fields = ('description',)
+    date_hierarchy = 'modified_date'
+    list_filter = ('created_at', 'modified_date',)
+
+
+@register(PlanModel)
+class PlanModelAdmin(ModelAdmin):
+    list_display = ('title', 'price',)
+    search_fields = ('title', 'description', 'price',)
+    raw_id_fields = ('type_of_plan',)
     date_hierarchy = 'modified_date'
     list_filter = ('created_at', 'modified_date',)
