bf2abf65d85dd4ff945be6476c45bc92f52fd9f9
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 21:21:39 +0300

    - Add field created_at to typeofplanmodel     - Add field modified_date to typeofplanmodel

--- service/submodels/plan.py
+++ service/submodels/plan.py
@@ -7,6 +7,8 @@ from django.db.models.fields.related import ForeignKey, OneToOneField
 class TypeOfPlanModel(Model):
     title = CharField(max_length=70)
     description = TextField(max_length=150, blank=True, null=True)
+    created_at = DateTimeField(auto_now_add=True, blank=True, null=True)
+    modified_date = DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.title
