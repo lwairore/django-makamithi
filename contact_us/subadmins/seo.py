f146e34bb693c444996a9e19e25e7b89bc29569b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 15:46:39 +0300

    - Create model admin 'ContactUsSEODetailsModelAdmin'.

---
+++ contact_us/subadmins/seo.py
@@ -0,0 +1,18 @@
+from contact_us.models import ContactUsSEODetailsModel
+from social_sharing.subadmins.seo_social_share_data import SeoSocialShareDataModelAdmin
+from django.contrib.admin import register
+
+
+@register(ContactUsSEODetailsModel)
+class ContactUsSEODetailsModelAdmin(SeoSocialShareDataModelAdmin):
+    def has_add_permission(self, request):
+        # check if generally has add permission
+        should_add_instance = super().has_add_permission(request)
+        # set add permission to False, if object already exists
+        if should_add_instance and ContactUsSEODetailsModel.objects.only('pk').exists():
+            should_add_instance = False
+
+        return should_add_instance
+
+    def has_delete_permission(self, request, obj=None):
+        return False
