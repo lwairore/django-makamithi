0afbfe2ce2d7dfd0baae108bb468430e296e785a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 14:22:26 +0300

    - Add private attribute '_serializer_class'.

--- contact_us/views/contact_info.py
+++ contact_us/views/contact_info.py
@@ -1,3 +1,4 @@
+from contact_us.serializers.contact_info import RetrieveContactInfoModelSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,3 +6,4 @@ from rest_framework.views import APIView
 
 class ListContactInfoAPIView(APIView):
     permission_classes = (AllowAny,)
+    _serializer_class = RetrieveContactInfoModelSerializer
