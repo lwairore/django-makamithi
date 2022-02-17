39d95c20dc33103eabded7ac0d62d2ab3c35d50f
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 14:25:44 +0300

    - Initialize constant '_LIST_CONTACT_INFO_URL_ROUTE'.

--- contact_us/suburls/contact_info.py
+++ contact_us/suburls/contact_info.py
@@ -4,3 +4,5 @@ from custom_utils.get_file_name_util import get_file_name
 CONTACT_INFO_ROOT_ROUTE = 'list/contact-info/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
+
+_LIST_CONTACT_INFO_URL_ROUTE = ''
