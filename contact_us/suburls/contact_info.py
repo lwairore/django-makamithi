61356fdb4d5f4e52b627c7e15c8a6c2c5dd1a365
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 14:34:08 +0300

    - Map api view 'ListContactInfoAPIView' to a 'APIView'.

--- contact_us/suburls/contact_info.py
+++ contact_us/suburls/contact_info.py
@@ -1,3 +1,5 @@
+from contact_us.views.contact_info import ListContactInfoAPIView
+from django.urls import path
 from custom_utils.get_file_name_util import get_file_name
 
 
@@ -6,3 +8,8 @@ CONTACT_INFO_ROOT_ROUTE = 'list/contact-info/'
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
 _LIST_CONTACT_INFO_URL_ROUTE = ''
+
+urlpatterns = [
+    path(_LIST_CONTACT_INFO_URL_ROUTE,
+         ListContactInfoAPIView.as_view()),
+]
