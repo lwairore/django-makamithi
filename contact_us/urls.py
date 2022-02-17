6cdb7bb29f66718ef88fe156a79e46ead51d071b
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 14:37:01 +0300

    - Initialize constant with private accessibility scope '_BASE_INCLUDE_PATH'.

--- contact_us/urls.py
+++ contact_us/urls.py
@@ -1,5 +1,10 @@
+from contact_us.suburls import URL_FILE_NAME
 from contact_us.apps import ContactUsConfig
 
 app_name = ContactUsConfig.name
+
+_BASE_INCLUDE_PATH = '{}.{}.'.format(
+    app_name,
+    URL_FILE_NAME)
 
 urlpatterns = []
