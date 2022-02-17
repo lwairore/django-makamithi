ac251a0925ae16ae2789674bf3c30ee107dad8b0
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 15:52:04 +0300

    - Include 'urlconfs' for section 'About SEO'.

--- contact_us/urls.py
+++ contact_us/urls.py
@@ -1,3 +1,5 @@
+from contact_us.suburls.seo import (CONTACT_US_SEO_DETAILS_ROOT_ROUTE,
+                                    URL_FILE_NAME_WITHOUT_EXTENSION as CONTACT_US_SEO_URL_FILE_NAME_WITHOUT_EXTENSION)
 from contact_us.suburls.contact_info import (CONTACT_INFO_ROOT_ROUTE,
                                              URL_FILE_NAME_WITHOUT_EXTENSION as CONTACT_INFO_URL_FILE_NAME_WITHOUT_EXTENSION)
 from django.urls import path, include
@@ -15,4 +17,9 @@ urlpatterns = [
     path(CONTACT_INFO_ROOT_ROUTE,
          include(
              _BASE_INCLUDE_PATH+CONTACT_INFO_URL_FILE_NAME_WITHOUT_EXTENSION)),
+
+    # Routes for section About SEO
+    path(CONTACT_US_SEO_DETAILS_ROOT_ROUTE,
+         include(
+             _BASE_INCLUDE_PATH+CONTACT_US_SEO_URL_FILE_NAME_WITHOUT_EXTENSION)),
 ]
