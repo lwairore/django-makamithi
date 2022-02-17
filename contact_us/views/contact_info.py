8c71af0c8edc9262c68f2fd802e506f6afd8d4fc
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 14:23:07 +0300

    - Update api view 'ListContactInfoAPIView'.

--- contact_us/views/contact_info.py
+++ contact_us/views/contact_info.py
@@ -7,3 +7,19 @@ from rest_framework.views import APIView
 class ListContactInfoAPIView(APIView):
     permission_classes = (AllowAny,)
     _serializer_class = RetrieveContactInfoModelSerializer
+
+    def _list_contact_info_kueryset(self):
+        contact_info_kueryset = FaqModel.objects\
+            .order_by()\
+            .only('question', 'answer',)\
+            .all()
+
+        return contact_info_kueryset
+
+    def get(self, request):
+        contact_info_kueryset = self._list_contact_info_kueryset()
+
+        contact_info_kueryset_serializer = self._serializer_class(
+            contact_info_kueryset, many=True)
+
+        return Response(contact_info_kueryset_serializer.data)
