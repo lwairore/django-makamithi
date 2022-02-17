8833873d1d2b8926186818eb670938f10f22c542
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-17 15:56:35 +0300

    - Import model admin 'TeamAreaSectionModelAdmin'.

--- about_us/subadmins/__init__.py
+++ about_us/subadmins/__init__.py
@@ -3,3 +3,4 @@ from .seo import AboutUsSEODetailsModelAdmin
 from .faq_section import FaqSectionModelAdmin
 from .faq import FaqModelAdmin
 from .what_we_do_section import WhatWeDoSectionModelAdmin
+from .team_area_section import TeamAreaSectionModelAdmin
