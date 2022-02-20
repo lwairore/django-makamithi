cd268433a0f69f89f858b606aa503f4fdbe8f8de
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-20 17:56:19 +0300

    - Update constant 'TEAM_ROOT_ROUTE'.

--- team/suburls/team.py
+++ team/suburls/team.py
@@ -3,7 +3,7 @@ from django.urls import path
 from custom_utils.get_file_name_util import get_file_name
 
 
-TEAM_ROOT_ROUTE = 'list/team/'
+TEAM_ROOT_ROUTE = 'list/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
