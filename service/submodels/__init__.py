49a0f39b3669320989cb0276ef1a5fbbe2180b3a
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-21 09:23:16 +0300

    - Create model ServiceAreaSectionModel

--- service/submodels/__init__.py
+++ service/submodels/__init__.py
@@ -2,3 +2,4 @@ from .service import ServiceModel
 from .pricing_area_section import PricingAreaSectionModel
 from .plan import TypeOfPlanModel, PlanModel
 from .service_seo import ServiceSEODetailsModel
+from .service_area_section import ServiceAreaSectionModel
--- service/suburls/service_seo.py
+++ service/suburls/service_seo.py
@@ -3,7 +3,7 @@ from service.views.service_seo import RetrieveServiceSEODetailsAPIView
 from custom_utils.get_file_name_util import get_file_name
 
 
-SERVICE_SEO_DETAILS_ROOT_ROUTE = 'retrieve/seo/service'
+SERVICE_SEO_DETAILS_ROOT_ROUTE = 'retrieve/seo/service/'
 
 URL_FILE_NAME_WITHOUT_EXTENSION = get_file_name(__file__)
 
