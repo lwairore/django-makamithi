ae2bf707543fa572e939f5efb9aa1b56170f3903
Lucas Wairore Karangu <kwairore@gmail.com>
2022-03-08 19:54:13 +0300

    - Push to heroku

--- makamithi/settings/production.py
+++ makamithi/settings/production.py
@@ -6,7 +6,7 @@ from .base import *
 import dj_database_url
 
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = config(
     'PRODUCTION_ALLOWED_HOSTS',
@@ -36,38 +36,38 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CORS_ORIGIN_WHITELIST = config(
-    'PRODUCTION_CORS_ORIGIN_WHITELIST',
-    cast=lambda v: tuple([s.strip().replace('\'', '') for s in v.split(',')]))
+# CORS_ORIGIN_WHITELIST = config(
+#     'PRODUCTION_CORS_ORIGIN_WHITELIST',
+#     cast=lambda v: tuple([s.strip().replace('\'', '') for s in v.split(',')]))
 
-# SECURE_SSL_REDIRECT redirects all HTTP requests to HTTPS (unless exempt).
-# This means your project will always try to use an encrypted connection.
-# You will need to have SSL configured on your server for this to work.
-# Note that if you have Nginx or Apache configured to do this already, this setting will be redundant.
-SECURE_SSL_REDIRECT = True
+# # SECURE_SSL_REDIRECT redirects all HTTP requests to HTTPS (unless exempt).
+# # This means your project will always try to use an encrypted connection.
+# # You will need to have SSL configured on your server for this to work.
+# # Note that if you have Nginx or Apache configured to do this already, this setting will be redundant.
+# SECURE_SSL_REDIRECT = True
 
-SESSION_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE tells the browser that cookies can only be handled over HTTPS.
-# This means cookies your project produces for activities, such as logins, will only work over an encrypted connection.
+# SESSION_COOKIE_SECURE = True
+# # SESSION_COOKIE_SECURE tells the browser that cookies can only be handled over HTTPS.
+# # This means cookies your project produces for activities, such as logins, will only work over an encrypted connection.
 
-CSRF_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE is the same as SESSION_COOKIE_SECURE but applies to your CSRF token.
-# CSRF tokens protect against Cross-Site Request Forgery.
-# Django CSRF protection does this by ensuring any forms submitted (for logins, signups, and so on) to your project were created by your project and not a third party.
+# CSRF_COOKIE_SECURE = True
+# # CSRF_COOKIE_SECURE is the same as SESSION_COOKIE_SECURE but applies to your CSRF token.
+# # CSRF tokens protect against Cross-Site Request Forgery.
+# # Django CSRF protection does this by ensuring any forms submitted (for logins, signups, and so on) to your project were created by your project and not a third party.
 
-SECURE_BROWSER_XSS_FILTER = True
-# SECURE_BROWSER_XSS_FILTER sets the X-XSS-Protection: 1; mode=block header on all responses that do not already have it.
-# This ensures third parties cannot inject scripts into your project.
-# For example, if a user stores a script in your database using a public field, when that script is retrieved and displayed to other users it will not run.
+# SECURE_BROWSER_XSS_FILTER = True
+# # SECURE_BROWSER_XSS_FILTER sets the X-XSS-Protection: 1; mode=block header on all responses that do not already have it.
+# # This ensures third parties cannot inject scripts into your project.
+# # For example, if a user stores a script in your database using a public field, when that script is retrieved and displayed to other users it will not run.
 
-X_FRAME_OPTIONS = 'DENY'
-# The default is 'SAMEORIGIN', but unless there is a good reason for your site to serve other parts of itself in a frame, you should change it to 'DENY'.
+# X_FRAME_OPTIONS = 'DENY'
+# # The default is 'SAMEORIGIN', but unless there is a good reason for your site to serve other parts of itself in a frame, you should change it to 'DENY'.
 
-SECURE_CONTENT_TYPE_NOSNIFF = True
-# You should consider enabling this header to prevent the browser from identifying content types incorrectly.
-# If not set to True, your pages will not be served with an 'X-Content-Type-Options: nosniff' header.
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# # You should consider enabling this header to prevent the browser from identifying content types incorrectly.
+# # If not set to True, your pages will not be served with an 'X-Content-Type-Options: nosniff' header.
 
-APPEND_SLASH = True
+# APPEND_SLASH = True
 
 prod_db = dj_database_url.config(
     conn_max_age=500,
