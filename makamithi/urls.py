6d4f7e2155d6596a016da0090f586e24edbc76ce
Lucas Wairore Karangu <kwairore@gmail.com>
2022-03-09 09:40:05 +0300

    - Unregister app 'Token'.

--- makamithi/urls.py
+++ makamithi/urls.py
@@ -31,9 +31,11 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic.base import RedirectView
 from django.shortcuts import redirect
+from rest_framework.authtoken.models import Token
 
+
+admin.site.unregister(Token)
 
 # default: "Django Administration"
 admin.site.site_header = 'Makamithi Administration'
